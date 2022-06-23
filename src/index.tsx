bc6f67ca2996f6652bfc204c5e4f7b2e26c4130a
Samuel Nishimwe <sam.nishimwe@yahoo.com>
2022-06-23 14:46:01 +0200

    feat: setup apollo client (#2)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
+import {ApolloCache, ApolloClient, InMemoryCache, useQuery, gql} from "@apollo/client"
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache()
+})
+
+client.query({
+  query: gql`
+    query shake {
+      hello
+    } `
+})
+.then(result => console.info(result))
 const container = document.getElementById('tree')!;
 
 const root = ReactDOMClient.createRoot(container)
