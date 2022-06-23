e79e3440633951ccb782b3cefda9e70d58ee38e0
ceelogre <sam.nishimwe@yahoo.com>
2022-06-23 14:59:13 +0200

    feat: render the test query

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import * as ReactDOMClient from 'react-dom/client'
-import {ApolloCache, ApolloClient, InMemoryCache, useQuery, gql} from "@apollo/client"
+import {ApolloProvider, ApolloClient, InMemoryCache, useQuery, gql} from "@apollo/client"
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache()
 })
 
+// test the graphql server
 client.query({
   query: gql`
     query shake {
@@ -14,6 +15,25 @@ client.query({
     } `
 })
 .then(result => console.info(result))
+.catch(error => console.error(error))
+
+// create an app component
+const App = () => {
+  const {data, loading, error} = useQuery(gql`
+    query shake {
+      hello
+    } `)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+  return <p>{data.hello}</p>
+}
+
+
+// render the app using apolloprovider
 const container = document.getElementById('tree')!;
 
-const root = ReactDOMClient.createRoot(container)
+const root = ReactDOMClient.createRoot(container).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
