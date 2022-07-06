88796061c006284832faab45fbab5e2c1ba6d5c3
himbazaJules <88627627+Julesbi@users.noreply.github.com>
2022-07-06 15:32:57 +0200

    chore(setup): setup tailwind (#5)
    finishes #dp-37

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,36 @@
-import React from 'react'
-import * as ReactDOMClient from 'react-dom/client'
-import {ApolloProvider, ApolloClient, InMemoryCache, useQuery, gql} from "@apollo/client"
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  useQuery,
+  gql,
+} from "@apollo/client";
+import "./index.css";
 
 const client = new ApolloClient({
-  uri: 'http://54.147.153.168',
-  cache: new InMemoryCache()
-})
+  uri: "http://54.147.153.168",
+  cache: new InMemoryCache(),
+});
 
 // create an app component
 const App = () => {
-  const {data, loading, error} = useQuery(gql`
+  const { data, loading, error } = useQuery(gql`
     query shake {
       hello
-    } `)
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
-  return <p>{data.hello}</p>
-}
-
+    }
+  `);
+  if (loading) return <p className="text-red-600 ">Loading...</p>;
+  if (error) return <p  className="text-red-600" >Error: {error.message}</p>;
+  return <p>{data.hello}</p>;
+};
 
 // render the app using apolloprovider
-const container = document.getElementById('tree')!;
+const container = document.getElementById("tree")!;
 
 const root = ReactDOMClient.createRoot(container).render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
-)
+);
