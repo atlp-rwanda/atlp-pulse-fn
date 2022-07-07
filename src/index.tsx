a1c57ab209e57489ecb5ce5283ab7151a76ee030
Samuel Shyaka Dushimimana <samyshyaka@gmail.com>
2022-07-07 13:43:32 +0200

    feat(config): configure linting (#7)
    - configure eslint
    - configure prettier
    - setup husky
    
    Finishes #DP-39

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,41 @@
-import React from "react";
-import * as ReactDOMClient from "react-dom/client";
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
 import {
   ApolloProvider,
   ApolloClient,
   InMemoryCache,
   useQuery,
   gql,
-} from "@apollo/client";
-import "./index.css";
+} from '@apollo/client';
+import './index.css';
 
 const client = new ApolloClient({
-  uri: "http://54.147.153.168",
+  uri: 'http://54.147.153.168',
   cache: new InMemoryCache(),
 });
 
 // create an app component
-const App = () => {
+function App() {
   const { data, loading, error } = useQuery(gql`
     query shake {
       hello
     }
   `);
   if (loading) return <p className="text-red-600 ">Loading...</p>;
-  if (error) return <p  className="text-red-600" >Error: {error.message}</p>;
+  if (error) {
+    return (
+<p className="text-red-600">
+Error:{error.message}</p>
+);
+  }
   return <p>{data.hello}</p>;
-};
+}
 
 // render the app using apolloprovider
-const container = document.getElementById("tree")!;
+const container = document.getElementById('tree')!;
 
 const root = ReactDOMClient.createRoot(container).render(
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>
+  </ApolloProvider>,
 );
