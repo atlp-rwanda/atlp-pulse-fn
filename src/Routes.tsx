ae52df38a39b7018efa46aa52812ba9d50fb8e0f
himbazaJules <88627627+Julesbi@users.noreply.github.com>
2022-07-08 23:32:38 +0200

    ft(orgAuth): Organization login page (#10)
    login with google
    
    login with SSO
    
    WIP #DP-49

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,6 +4,8 @@ import OrgRegister from './pages/OrgRegister';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Orglogin from './pages/Organization/Orglogin';
+import Orggooglelogin from './pages/Organization/Orggooglelogin';
 
 function MainRoutes() {
   return (
@@ -13,6 +15,8 @@ function MainRoutes() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register-organization" element={<OrgRegister />} />
+          <Route path="/org-google-login" element={<Orggooglelogin />} />
+          <Route path="/org-login" element={<Orglogin />} />
         </Routes>
         <Footer />
       </Router>
