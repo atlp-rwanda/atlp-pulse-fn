238dcbe790531bc82303d980ced906b8ee6e5213
samuel-shyaka-dus <samyshyaka@gmail.com>
2022-07-11 10:48:11 +0200

    chore(config): configure CI/CD
    - GitHub Actions is configured
    - Heroku Pipeline is confgured
    - GitHub Actions and coveralls badges have been added to the repo
    
    Finishes #DP-55

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/test/jest/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/test/jest/__mocks__/styleMock.js',
+  },
 };
