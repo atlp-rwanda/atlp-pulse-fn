e10ea150dcad88131cd3eea10a0000bf4b7b5aa6
Samuel Shyaka Dushimimana <samyshyaka@gmail.com>
2022-07-12 10:37:52 +0200

    chore(config): configure CI/CD (#17)
    - GitHub Actions is configured
    - Heroku Pipeline is confgured
    - GitHub Actions and coveralls badges have been added to the repo
    
    Finishes #DP-55

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+[![CI](https://github.com/atlp-rwanda/atlp-pulse-fn/actions/workflows/main.yml/badge.svg)](https://github.com/atlp-rwanda/atlp-pulse-fn/actions/workflows/main.yml) [![Coverage Status](https://coveralls.io/repos/github/atlp-rwanda/atlp-pulse-fn/badge.svg?branch=develop)](https://coveralls.io/github/atlp-rwanda/atlp-pulse-fn?branch=develop)
+
 # atlp-pulse-fn
+
+The staging app can be found [here](https://dev-pulse-staging.herokuapp.com/)
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -3,6 +3,21 @@ import renderer from 'react-test-renderer';
 import App from '../App';
 
 describe('App test ', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // Deprecated
+        removeListener: jest.fn(), // Deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      }))
+    });
+  });
   it('Should render app', () => {
     const elem = renderer.create(<App />).toJSON();
     expect(elem).toMatchSnapshot();
