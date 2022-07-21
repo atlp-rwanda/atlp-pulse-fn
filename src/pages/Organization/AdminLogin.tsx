e1069817f42e34d2a7be53da94e013fc9ffa9055
itisWasp <mugishaisrael18@gmail.com>
2022-07-21 12:59:45 +0200

    feature(Frontend Validation): Added Validations on Forms
    Reconfigured Validation
    
    Removed Snapshots
    
    Reconfigured Husky
    
    Fixed Lint
    
    Updated Snapshots
    
    Removed The uncessary dependencies

--- src/pages/Organization/AdminLogin.tsx
+++ src/pages/Organization/AdminLogin.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useForm } from 'react-hook-form';
 import { FaGoogle, FaRegEnvelope, FaRegEye } from 'react-icons/fa';
@@ -8,18 +8,23 @@ import { MdLockOutline } from 'react-icons/md';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { UserContext } from '../../hook/useAuth';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function AdminLogin() {
   const { t } = useTranslation();
   const [passwordShown, setPasswordShown] = useState(false);
-  const tooglePassword = () => {
-    setPasswordShown(!passwordShown);
-  };
+
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   }: any = useForm();
+
+  const tooglePassword = () => {
+    setPasswordShown(!passwordShown);
+  };
+
   const { login } = useContext(UserContext);
   const navigate = useNavigate();
 
@@ -35,37 +40,37 @@ function AdminLogin() {
   };
 
   return (
-    <div className="md:flex md:flex-col md:items-center md:justify-center w-full  grow  text-center py-2  dark:bg-dark-bg bg-gray-100  sm:flex sm:flex-row sm:items-center sm:justify-center">
-      <div className="md:rounded-2xl md:shadow-2xl md:flex md:w-2/3 mt-16 md:max-w-4xl sm:max-w-xl sm:rounded-none sm:shadow-none dark:shadow-2xl mb-8">
-        <div className="md:w-3/5 md:p-5 sm:w-full sm:p-2 dark:bg-dark-frame-bg  dark:rounded-none">
+    <div className="w-full py-2 text-center bg-gray-100 md:flex md:flex-col md:items-center md:justify-center grow dark:bg-dark-bg sm:flex sm:flex-row sm:items-center sm:justify-center">
+      <div className="mt-16 mb-8 md:rounded-2xl md:shadow-2xl md:flex md:w-2/3 md:max-w-4xl sm:max-w-xl sm:rounded-none sm:shadow-none dark:shadow-2xl">
+        <div className="md:w-3/5 md:p-5 sm:w-full sm:p-2 dark:bg-dark-frame-bg dark:rounded-none">
           <div className="py-10 sm:py-8 ">
             <h2 className="text-2xl font-bold text-primary dark:text-dark-text-fill ">
               {t('Sign in using')}
             </h2>
-            <div className="border-2 w-10 bg-primary border-primary inline-block mb-2" />
+            <div className="inline-block w-10 mb-2 border-2 bg-primary border-primary" />
             <div className="flex justify-center my-2">
               <a
                 href="#link"
-                className="border-2 border-gray-200 rounded-full p-3 mx-1"
+                className="p-3 mx-1 border-2 border-gray-200 rounded-full"
               >
                 <FaGoogle className="text-sm dark:text-white" />
               </a>
             </div>
-            <p className="text-gray-400 my-3 dark:text-dark-text-fill ">
+            <p className="my-3 text-gray-400 dark:text-dark-text-fill ">
               {t('or use your email account')}
             </p>
             <div className="flex flex-col items-center">
               <form action="#none" onSubmit={handleSubmit(onSubmit)}>
-                <div className="bg-gray-100 w-64 p-2 flex items-center mb-2 dark:bg-dark-bg ">
-                  <FaRegEnvelope className="text-gray-400 mr-2" />
+                <div className="flex items-center w-64 p-2 mb-2 bg-gray-100 dark:bg-dark-bg ">
+                  <FaRegEnvelope className="mr-2 text-gray-400" />
                   <input
                     type="email"
                     {...register('email', { required: 'Email is required' })}
                     placeholder={t('Email')}
-                    className="bg-gray-100 outline-none text-sm flex-1 text-gray-400 dark:border-white dark:bg-dark-bg dark:text-white "
+                    className="flex-1 text-sm text-gray-400 bg-gray-100 outline-none dark:border-white dark:bg-dark-bg dark:text-white "
                   />
                 </div>
-                <div className="text-left mb-1">
+                <div className="mb-1 text-left">
                   {errors.email && (
                     <small className="text-red-600">
                       {errors.email.message}
@@ -73,15 +78,15 @@ function AdminLogin() {
                   )}
                 </div>
 
-                <div className="bg-gray-100 w-64 p-2 flex items-center rounded mb-2 dark:border-white dark:bg-dark-bg">
-                  <MdLockOutline className="text-gray-400 mr-2 " />
+                <div className="flex items-center w-64 p-2 mb-2 bg-gray-100 rounded dark:border-white dark:bg-dark-bg">
+                  <MdLockOutline className="mr-2 text-gray-400 " />
                   <input
                     type={passwordShown ? 'text' : 'password'}
                     {...register('password', {
                       required: 'Password is required',
                     })}
                     placeholder={t('Password')}
-                    className="bg-gray-100 outline-none text-sm flex-1 text-gray-400 dark:border-white dark:bg-dark-bg dark:text-white"
+                    className="flex-1 text-sm text-gray-400 bg-gray-100 outline-none dark:border-white dark:bg-dark-bg dark:text-white"
                   />
                   <div className="text-gray-400 cursor-pointer onClick= {()=> handleShowPassword}">
                     {passwordShown ? (
@@ -91,14 +96,14 @@ function AdminLogin() {
                     )}
                   </div>
                 </div>
-                <div className="text-left mb-1">
+                <div className="mb-1 text-left">
                   {errors.password && (
                     <small className="text-red-600">
                       {errors.password.message}
                     </small>
                   )}
                 </div>
-                <div className="flex w-64 justify-between rounded mb-5 mt-5">
+                <div className="flex justify-between w-64 mt-5 mb-5 rounded">
                   <label
                     htmlFor="checkbox"
                     className="flex items-center text-xs dark:text-dark-text-fill "
@@ -106,7 +111,7 @@ function AdminLogin() {
                     <input
                       type="checkbox"
                       name="remember"
-                      className="mr-1  dark:text-dark-text-fill dark:border-white dark:bg-dark-frame-bg"
+                      className="mr-1 dark:text-dark-text-fill dark:border-white dark:bg-dark-frame-bg"
                     />
                     Remember me
                   </label>
@@ -119,13 +124,13 @@ function AdminLogin() {
                 </div>
                 <button
                   type="submit"
-                  className="border-2 border-primary text-primary rounded-full lg:px-12 lg:py-2 inline-block sm:px-4 sm:py-1  md:font-semibold sm:mt-2 sm:font-medium hover:bg-primary hover:text-white  "
+                  className="inline-block border-2 rounded-full border-primary text-primary lg:px-12 lg:py-2 sm:px-4 sm:py-1 md:font-semibold sm:mt-2 sm:font-medium hover:bg-primary hover:text-white "
                 >
                   Sign In
                 </button>
               </form>
             </div>
-            <div className="md:hidden mt-2 text-xs text-center dark:text-dark-text-fill">
+            <div className="mt-2 text-xs text-center md:hidden dark:text-dark-text-fill">
               {t('First time here?')}
               <Link to="/register-organization" className="mx-1">
                 <a href="#link" className="text-primary">
@@ -136,11 +141,11 @@ function AdminLogin() {
             </div>
           </div>
         </div>
-        <div className="md:w-2/5 bg-primary text-white  lg:rounded-tr-2xl lg:rounded-br-2xl md:py-36 md:px-12 sm:w-full sm:rounded-none sm:px-12 sm:py-4 md:block sm:hidden dark:text-dark-text-fill ">
+        <div className="text-white md:w-2/5 bg-primary lg:rounded-tr-2xl lg:rounded-br-2xl md:py-36 md:px-12 sm:w-full sm:rounded-none sm:px-12 sm:py-4 md:block sm:hidden dark:text-dark-text-fill ">
           <h2 className="lg:text-3xl md:text-xl md:font-bold md:mb-2 ">
             {t('Hello, Friend!')}
           </h2>
-          <div className="border-2 w-10 bg-white border-white inline-block mb-2" />
+          <div className="inline-block w-10 mb-2 bg-white border-2 border-white" />
           <p className="lg:mb-10 lg:text-center md:text-medium sm:text-center sm:mb-2 sm:text-sm md:text-justify dark:text-dark-text-fill ">
             {t('Sign_in_page_paragraph')}
           </p>
