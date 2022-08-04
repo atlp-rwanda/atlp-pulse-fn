f4b6688b2d026365635863971c13f674376cf65e
Fabrice Hafashimana <28519395+feyton@users.noreply.github.com>
2022-08-04 14:24:33 +0200

    Fix: Login redirection (#49)
    - The user is redirected to the requested pages
    - The user receives a warning when accessing protected route

--- src/pages/tests/AdminLogin.test.tsx
+++ src/pages/tests/AdminLogin.test.tsx
@@ -1,7 +1,22 @@
+import { fireEvent, render } from '@testing-library/react';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
+import UserProvider from '../../hook/useAuth';
 import AdminLogin from '../Organization/AdminLogin';
+
+const mockedUsedLocation = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: () => mockedUsedLocation,
+}));
+
+const mockUseLocationValue = {
+  pathname: '/testroute',
+  search: '',
+  hash: '',
+  state: '/dashboard/trainee',
+};
 
 describe('Admin Login', () => {
   it('Should render', () => {
@@ -13,5 +28,41 @@ describe('Admin Login', () => {
       )
       .toJSON();
     expect(elem).toMatchSnapshot();
+  });
+});
+
+describe('Admin login with error', () => {
+  jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useLocation: jest.fn().mockImplementation(() => mockUseLocationValue),
+  }));
+  it('Should render with login error message', async () => {
+    const { getByTestId, findByText } = render(
+      <MemoryRouter>
+        <UserProvider>
+          <AdminLogin />
+        </UserProvider>
+      </MemoryRouter>,
+    );
+    fireEvent.submit(getByTestId('loginForm'));
+    expect(await findByText('Email is required')).toBeInTheDocument();
+  });
+  it('Should submit the form', async () => {
+    const { getByTestId, findByText } = render(
+      <MemoryRouter>
+        <UserProvider>
+          <AdminLogin />
+        </UserProvider>
+      </MemoryRouter>,
+    );
+
+    fireEvent.input(getByTestId('email'), {
+      target: { value: 'admin@me.com' },
+    });
+    fireEvent.input(getByTestId('password'), {
+      target: { value: '12345678' },
+    });
+    fireEvent.submit(getByTestId('loginForm'));
+    expect(await findByText('Sign In')).toBeInTheDocument();
   });
 });
