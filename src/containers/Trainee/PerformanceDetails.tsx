bb58d5bf10787c6911b81f432f2b35e751affbee
Chance Desire IRADUKUNDA <67952319+chanceDira@users.noreply.github.com>
2022-08-11 15:14:53 +0200

    fix(comment): changing design (#56)
    Build comment card
    Dark&light mode
    Translation
    (WIP)

--- src/containers/Trainee/PerformanceDetails.tsx
+++ src/containers/Trainee/PerformanceDetails.tsx
@@ -10,10 +10,10 @@ function PerformanceDetails() {
   const{t} = useTranslation();
   const navigate = useNavigate();
   return (
-    <div>
+    <div className='bg-neutral-100 dark:bg-dark-frame-bg'>
       <TraineePerformanceDetails />
       <TraineeComments />
-      <div className="bg-neutral-100 dark:bg-dark-frame-bg">
+      {/* <div className="bg-neutral-100 dark:bg-dark-frame-bg">
         <button
           onClick={() => navigate(-1)}
           className="flex lg:ml-72  sm:ml-14 bg-primary px-4 md:py-2 sm:py-1 md:mt-0 sm:mt-[-200px]  rounded-md text-white font-semibold cursor-pointer"
@@ -21,7 +21,7 @@ function PerformanceDetails() {
           <IoIosArrowBack className="mt-1 mr-1" />
           {t('Back')}
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
