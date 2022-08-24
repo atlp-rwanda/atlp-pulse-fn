8da871abe21cb67d93811e9e3f9f2ec6d6c9e807
Fabrice Hafashimana <28519395+feyton@users.noreply.github.com>
2022-08-24 11:39:08 +0200

    Fix( load time): Review apps (#65)
    - Adjust environment variables

---
+++ src/Skeletons/Square.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Square() {
+  return (
+    <div>
+      <div className="bg-gray-300 h-full animate-pulse hidden md:block rounded-md w-full ml-2" />
+    </div>
+  );
+}
--- src/containers/Routes.tsx
+++ src/containers/Routes.tsx
@@ -1,29 +1,29 @@
 /* eslint-disable */
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import OrgRegister from '../pages/OrgRegister';
 import Home from '../pages/Home';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Orglogin from '../pages/Organization/Orglogin';
-import Adminlogin from '../pages/Organization/AdminLogin';
-import PricingForm from '../components/PricingForm';
-import Pricing from '../pages/Pricing';
-import Orggooglelogin from '../pages/Organization/Orggooglelogin';
-import Calendar from '../components/Calendar';
 import Error from './../pages/Error';
+import Skeleton from '../components/Skeleton';
+const OrgRegister = React.lazy(() => import('../pages/OrgRegister'));
+const Orglogin = React.lazy(() => import('../pages/Organization/Orglogin'));
+const Adminlogin = React.lazy(() => import('../pages/Organization/AdminLogin'));
 
 function MainRoutes() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register-organization" element={<OrgRegister />} />
-        <Route path="/org-login" element={<Orglogin />} />
-        <Route path="/login-admin" element={<Adminlogin />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <Suspense fallback={<Skeleton />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register-organization" element={<OrgRegister />} />
+          <Route path="/org-login" element={<Orglogin />} />
+          <Route path="/login-admin" element={<Adminlogin />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
+
       <Footer />
     </div>
   );
