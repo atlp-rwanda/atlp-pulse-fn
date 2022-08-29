6911d081c57b3e88d96d3f77c9eb1a318a850e89
Rukundo Kevin <kevinrukundo1@gmail.com>
2022-08-29 12:16:14 +0200

    Fix: profile options
    - added link to profile option items that didn't point to any page.
    
    Finishes #DP-99

--- src/components/ProfileDropdown.tsx
+++ src/components/ProfileDropdown.tsx
@@ -22,7 +22,7 @@ function ProfileDropdown({
         onKeyDown={handleShowProfileDropdown}
         data-testid="keyClick"
       />
-      <div className="absolute top-[60px] right-0 left-0 ml-auto px-2 md:mx-0 md:left-auto md:right-[6px] z-2  w-full max-w-[220px] h-[calc(100%-70px)]">
+      <div className="absolute top-[60px] right-0 left-0 ml-auto px-2 md:mx-0 md:left-auto md:right-[10px] z-2  w-full max-w-[220px] h-[calc(100%-70px)]">
         <div className="flex flex-col flex-nowrap w-full h-max max-h-full bg-white shadow-xl border  dark:border-0 dark:bg-dark-tertiary rounded-[20px]">
           <div className="flex flex-col w-full p-3 border-border-dark dark:border-white border-b-[0.5px]">
             <p className="font-bold dark:text-white">John Doe</p>
@@ -37,15 +37,12 @@ function ProfileDropdown({
             <div className="w-full border-border-dark dark:border-white border-b-[0.5px]">
               <div className="flex flex-row justify-between align-center gap-x-[20px] ">
                 <div className="flex flex-col w-full gap-[5px] cursor-pointer">
-                  <p className="font-semibold text-gray-600 dark:text-white px-4 py-2 hover:bg-gray-600 hover:text-gray-200 dark:hover:bg-gray-300 dark:hover:text-gray-900">
-                    <Link to="/dashboard/profile">{t('Profile')}</Link>
-                  </p>
-                  <p className="font-semibold text-gray-600 dark:text-white px-4 py-2 hover:bg-gray-600 hover:text-gray-200 dark:hover:bg-gray-300 dark:hover:text-gray-900">
-                    <Link to="#link">{t('Preference')}</Link>
-                  </p>
-                  <p className="font-semibold text-gray-600 dark:text-white px-4 py-2 hover:bg-gray-600 hover:text-gray-200 dark:hover:bg-gray-300 dark:hover:text-gray-900">
-                    <Link to="settings">{t('Privacy')}</Link>
-                  </p>
+                  <Link onClick={handleShowProfileDropdown} to="/dashboard/profile" className="font-semibold text-gray-600 dark:text-white px-4 py-2 hover:bg-gray-600 hover:text-gray-200 dark:hover:bg-gray-300 dark:hover:text-gray-900">
+                    {t('Profile')}
+                  </Link>
+                  <Link onClick={handleShowProfileDropdown} to="settings" className="font-semibold text-gray-600 dark:text-white px-4 py-2 pb-4 hover:bg-gray-600 hover:text-gray-200 dark:hover:bg-gray-300 dark:hover:text-gray-900">
+                    {t('Preferences')}
+                  </Link>
                 </div>
               </div>
             </div>
