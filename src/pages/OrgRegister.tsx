92ccf059b08ddc3324a2e1e6dd257e26e96f1202
ishimweeli <ishimweeli0@gmail.com>
2022-09-24 08:43:49 +0200

    my first commits

--- src/pages/OrgRegister.tsx
+++ src/pages/OrgRegister.tsx
@@ -48,6 +48,7 @@ export default function OrgRegister() {
   };
 
   return (
+
     <div className="md:flex md:flex-col md:items-center md:justify-center w-full min-h-screen -mt-3 -mb-16 xl:-mt-14  xl:-mb-28 text-left dark:bg-dark-frame-bg bg-gray-100 sm:flex sm:flex-row sm:items-center sm:justify-center">
       <div className="bg-white sm:rounded-2xl py-8 md:py-0 md:shadow-2xl md:flex xl:w-3/4 md:w-[90%] md:max-w-4xl sm:w-full px-2 md:px-0 mx-20 md:mx-0 sm:shadow-none dark:shadow-2xl ">
         <div className="2xl:w-3/5 md:w-screen md:pl-14 lg:pl-32 xl:pl-24 py-20 md:py-8 sm:w-full sm:p-2 dark:bg-dark-bg dark:rounded-tl-xl dark:rounded-bl-xl">
@@ -170,6 +171,7 @@ export default function OrgRegister() {
           <h3 className="font-bold text-left text-white text-lg mb-2 px-5">
             {t("Your organization's link")}
           </h3>
+
           <button
             type="button"
             className="rounded bg-white text-primary ml-20 px-3 font-bold py-2 mt-3"
