6c76c71181de1eaca00f54899ad5be7fea52454d
Elissa Design <82683808+ElissaDesign@users.noreply.github.com>
2022-11-29 11:44:26 +0200

    chore(fix):fixed the dashboard redirect&coming soon page. (#183)
    chore(fix):fixed the dashboard redirect&coming soon page.
    
    chore(fix):fixed the dashboard redirect&coming soon page.
    
    chore(fix):fixed the dashboard redirect&coming soon page.
    
    chore(fix): fixed the dashboard redirect&coming soon page
    
    chore(fix): fixed the dashboard redirect
    
    chore(link):added stagging link
    
    chore(fix): fixed thebug in redirect
    
    chore(fix): fixed the user login
    
    chore(fix): fixed the trainee and manager redirect
    
    chore(fix): fixed redirect tr
    
    chore(fix ): redirect trainee
    
    chore(fix): changed the state
    
    chore(fix): changed the state
    
    Co-authored-by: Ihimbazwe Elissa <ihimbazwelisa@gmail.com>

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { t } from 'i18next';
 import Chart from '../components/Chart';
 import Card from '../components/Card';
+// eslint-disable-next-line import/no-useless-path-segments
 import useDocumentTitle from '../hook/useDocumentTitle';
+import Comingsoon from './Comingsoon';
 
 function SupAdDashboard() {
   useDocumentTitle('Dashboard');
@@ -10,13 +12,16 @@ function SupAdDashboard() {
     <div className="flex flex-col grow bg-light-bg dark:bg-dark-frame-bg">
       <div className="flex flex-row pb-8 justify-center">
         <div className="lg:ml-56 w-[90%] pt-[8vh] h-[100%]">
-          <div className="grid grid-cols-2 mb-12 md:mb-24 lg:mb-0 lg:grid-cols-4">
+          {/* <div className="grid grid-cols-2 mb-12 md:mb-24 lg:mb-0 lg:grid-cols-4">
             <Card text={t('Coordinators')} number={47} />
             <Card text={t('Trainees')} number={560} />
             <Card text={t('Cohorts')} number={8} />
             <Card text={t('Performance')} number={4} />
           </div>
-          <Chart title={t('Overall performance')} />
+          <Chart title={t('Overall performance')} /> */}
+          
+          <Comingsoon title='Dashboard'/>
+
         </div>
       </div>
     </div>
--- src/pages/TraineeDashboard.tsx
+++ src/pages/TraineeDashboard.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { t } from 'i18next';
 import Chart from '../components/Chart';
 import Card from '../components/Card';
+import Comingsoon from './Comingsoon';
+
 
 function SupAdDashboard() {
   return (
     <div className="flex flex-col grow bg-light-bg dark:bg-dark-frame-bg">
       <div className="flex flex-row pb-8 justify-center">
         <div className="lg:ml-44 w-[90%] pt-[8vh] h-[100%]">
-          <div className="grid grid-cols-2 mb-12 md:mb-24 lg:mb-0 lg:grid-cols-4">
+          {/* <div className="grid grid-cols-2 mb-12 md:mb-24 lg:mb-0 lg:grid-cols-4">
             <Card text="Cohort" number={5} />
             <Card text="Curret phase" number={2} />
             <Card text="Perfomance" number={2} />
             <Card text="Attendance" number={1} />
           </div>
-          <Chart title={t('Overall performance')} />
+          <Chart title={t('Overall performance')} /> */}
+
+          <Comingsoon title='Dashboard'/>
         </div>
       </div>
     </div>
