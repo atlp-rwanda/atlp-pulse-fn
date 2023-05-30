c3ca39064c68272e472a9e3010d25a656298f533
uwawetera <uwaweteraviv@gmail.com>
2023-05-31 01:05:38 +0300

    changes to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,11 +17,32 @@ function LandingPage() {
     <div className="hero">
       <Header styles="bg-opacity-50 dark:bg-opacity-50" />
 
-      <div className="hero w-full min-h-full flex flex-col justify-center grow items-center py-20">
+      <div className="hero w-full min-h-full   flex flex-col justify-center grow items-center py-20">
         <div className="grid h-full lg:grid-cols-2 w-full justify-around content-center pb-6">
           <div className="flex h-full flex-col items-center justify-start  w-full px-4 py-2">
-            <h1 className="mt-28 sm:mt-20 text-3xl sm:text-4xl md:pl-10 md:text-6xl font-bold text-white dark:text-dark-text-fill font-lexend text-center md:text-left">
-              {t('Welcome to DevPulse')}
+            <h1 className="mt-10 sm:mt-10 text-2xl sm:text-3xl md:pl-10 md:text-5xl font-bold text-white dark:text-dark-text-fill font-lexend md:text-left">
+              The brilliant management platform around the word
+            </h1>
+
+            {!user?.auth ? (
+              <div className="w-full items-center mt-6 ml-14">
+                <Link to="/signup/org">
+                  <Button
+                    variant="primary"
+                    size="lg"
+                    style="mt-12 lg:mt-0 px-8 text-xl font-bold uppercase my-4"
+                  >
+                    {' '}
+                    {t('Join Us')}{' '}
+                  </Button>
+                </Link>
+              </div>
+            ) : (
+              ' '
+            )}
+
+            {/* <h1 className="mt-28 sm:mt-20 text-2xl sm:text-3xl md:pl-10 md:text-5xl font-bold text-white dark:text-dark-text-fill font-lexend text-center md:text-left"> */}
+            {/* {t('Welcome to DevPulse')}
             </h1>
             <p className="text-2xl md:text-4xl lg:text-3xl mt-8 md:mt-8 lg:mt-8 w-full sm:w-3/4 md:w-4/5 lg:w-full md:px-0 lg:px-8 text-white dark:text-dark-text-fill font-sans text-center">
               {t('The number one platform for')}
@@ -52,7 +73,7 @@ function LandingPage() {
             </div>
             ) : (
               ' '
-            )}
+            )} */}
           </div>
         </div>
       </div>
