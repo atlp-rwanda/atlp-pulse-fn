8bb1638e5b3ebf8ac30a254ae8e5fd2d9e665fca
Mystica <r.dukuzeyez@alustudent.com>
2023-07-19 13:34:23 +0200

    ch(fix sign up):  fixing sign up
    - google is removed
    -Date of birth is validated to be at least 18 years
    -Email field is automatically filled and uneditable ( collect the email
    from the sign up link sent to them)
    -Password should not be too easy. (Should not be in the first 100 most
    common passwords: https://tinyurl.com/wiki-100)
    
    [Finishes #42-signup-fix]

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -63,7 +63,7 @@ function DataTable({ data, columns, title, loading, className }: TableData) {
           <input
             defaultValue={globalFilter || ''}
             placeholder="Filter"
-            className="px-5 py-2 mt-4 font-sans text-xs border rounded outline-none border-primary dark:bg-dark-tertiary dark:text-white w-52 md:w-96"
+            className="px-5 py-2 mt-4 font-sans text-xs border border-primary rounded outline-none dark:bg-neutral-600 dark:text-white w-52 md:w-96"
             /* istanbul ignore next */
             onChange={(e) => setGlobalFilter(e.target.value)}
           />
