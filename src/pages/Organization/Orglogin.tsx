a6c954906bd79fe08b4303c4260804c5ed6753f3
marita <marthairadukunda1@gmail.com>
2023-08-03 15:52:11 +0300

    feat(orgName case insensitive): devpulse org login (#225)

--- src/pages/Organization/Orglogin.tsx
+++ src/pages/Organization/Orglogin.tsx
@@ -9,10 +9,34 @@ import Button from '../../components/Buttons';
 import useDocumentTitle from '../../hook/useDocumentTitle';
 import LOGIN_ORGANIZATION_MUTATION from './LoginOrganisationMutation';
 import ButtonLoading from '../../components/ButtonLoading';
+import './orgName.css';
+// import { input } from '@testing-library/user-event/dist/types/event/input';
 
 function Orglogin() {
   useDocumentTitle('Login');
   const { t } = useTranslation();
+  const [name, setName] = useState('');
+  
+
+  const [inputWidth, setInputWidth] = useState('150px'); // Adjust the width as needed
+  const handleNameChange = (e: { target: { value: any; }; }) => {
+    const newName = e.target.value;
+    setName(newName);
+
+    // Calculate the width based on the length of the entered text
+    const newWidth = `${Math.max(newName.length * 9)}px`; // Adjust minimum width here
+    setInputWidth(newWidth);
+  };
+
+  
+
+  const formatVariable = async (e: any) => {
+    const value: string = String(e.target.value);
+    if (!value.includes('.devpulse.org')) {
+      e.target.value = `${e.target.value}.devpulse.org`;
+      setName(e.target.value);
+    }
+  };
   const [OrgLogin, { loading }] = useMutation(LOGIN_ORGANIZATION_MUTATION);
   const {
     register,
@@ -31,8 +55,14 @@ function Orglogin() {
       onCompleted({ loginOrg }) {
         /* istanbul ignore next */
         localStorage.setItem('orgToken', loginOrg.token);
+        let value: string = String(orgInput.name);
+        if (!value.includes('.devpulse.org')) {
+          value = `${orgInput.name}.devpulse.org`;
+        }
+        // console.log(value)
         /* istanbul ignore next */
-        localStorage.setItem('orgName', orgInput.name);
+        // TODO:
+        localStorage.setItem('orgName', value);
         toast.success('Welcome! Sign in to Continue');
         navigate('/users/login');
       },
@@ -44,6 +74,9 @@ function Orglogin() {
       },
     });
   };
+  const names = name.replace(/ /gi, '').toLowerCase();
+  // console.log('Bye Brother hey', names)
+  const completeOrgUrl = `${names}`;
 
   return (
     <div className="grow bg-neutral-100 dark:bg-dark-frame-bg flex flex-col justify-center font-sans">
@@ -59,17 +92,31 @@ function Orglogin() {
           className="space-y-6 mt-4"
           onSubmit={handleSubmit(onSubmit)}
         >
-          <div>
-            <input
-              type="text"
-              data-testid="orgName"
-              {...register('name', {
-                required: 'Organisation name is required',
-              })}
-              placeholder={t('your-organization.devpulse.co')}
-              className="w-full p-2 border border-primary rounded mt-1 dark:bg-dark-bg"
-            />
-          </div>
+
+<div className="orgName">
+      <input
+        placeholder="Your-organization"
+        type="text"
+        value={name}
+        data-testid="orgName"
+        {...register('name', {
+          required: 'Organisation name is required',
+        })}
+        onChange={handleNameChange}
+        style={{ width: inputWidth }} // Set the width dynamically
+        className="inputStyle inputOne w-full border-primary rounded mt-1 dark:bg-dark-bg"
+      />
+      <input
+        placeholder=".devpulse.org"
+        disabled
+        className="inputStyle inputTwo w-full p-2 border border-primary rounded mt-1 dark:bg-dark-bg"
+      />
+    </div>
+
+
+
+
+
           <div className="-mt-6">
             {errors.name && (
               <small className="text-red-600">{errors.name.message}</small>
