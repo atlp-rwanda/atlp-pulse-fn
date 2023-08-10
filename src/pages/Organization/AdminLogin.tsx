24c1f9fe94d8b725b245e730a23d9bda63f8326a
edwinnambaje <nambajeeedwin@gmail.com>
2023-08-10 02:45:34 +0200

    added new theme

--- src/pages/Organization/AdminLogin.tsx
+++ src/pages/Organization/AdminLogin.tsx
@@ -18,10 +18,13 @@ import Button from '../../components/Buttons';
 import { UserContext } from '../../hook/useAuth';
 import useDocumentTitle from '../../hook/useDocumentTitle';
 import LOGIN_MUTATION from './LoginMutation';
+import functionTree from '../../assets/Functionality_Tree.svg';
+import pulseStars from '../../assets/Property 1=Logo_flie (1).svg';
 
 function AdminLogin() {
   const orgToken: any = localStorage.getItem('orgToken');
   const orgName: any = localStorage.getItem('orgName');
+  console.log(orgName);
 
   useDocumentTitle('Login');
   const { t } = useTranslation();
@@ -135,127 +138,163 @@ function AdminLogin() {
   };
 
   return (
-    <div className="w-full h-full py-2 text-center bg-gray-100 dark:bg-dark-frame-bg sm:flex sm:items-center sm:justify-center">
-      <div className="mt-20 mb-8 bg-indigo-100 md:rounded-xl md:shadow-xl md:w-full sm:max-w-xl dark:bg-dark-bg sm:rounded-none sm:shadow-none dark:shadow-2xl">
-        <div className="py-10 sm:py-8 ">
-          <h2 className="text-2xl font-bold text-primary dark:text-dark-text-fill ">
-            {t('Welcome to')}{' '}
-            {orgName
-              ? orgName.charAt(0).toUpperCase() + orgName.slice(1).toLowerCase()
-              : ''}
-          </h2>
-          <div className="border-[1px] w-10 bg-primary border-primary inline-block mb-2" />
-          <div className="text-sm text-center dark:text-dark-text-fill">
-            <Link
-              to="/login/org"
-              className="mx-1 text-primary"
-              data-testid="switch"
-            >
-              {t('Switch')} {t('your organization')}
-            </Link>
+    <div className="grow  dark:bg-dark-frame-bg bg-indigo-50 flex flex-row font-sans">
+      <div className="hidden lg:flex w-[50%] h-full  flex-col justify-center items-center mt-10 ">
+        <div className=" flex flex-col justify-center items-center ">
+          <div className="relative flex flex-row justify-center">
+            <img
+              src={pulseStars}
+              alt="pulses"
+              className="absolute inset-0 w-[41rem] h-[12rem] dark:hidden"
+            />
+            <p className="relative w-[70%] text-gray-700 text-[1.9em] p-5 text-center italic font-bold  dark:text-dark-text-fill">
+              {t('Boost your organization')}
+            </p>
           </div>
 
-          <div className="flex flex-col items-center">
-            <form
-              action="#none"
-              onSubmit={handleSubmit(onSubmit)}
-              data-testid="loginForm"
-            >
-              {errors.password &&
-              errors.password.message === t('Invalid credentials') ? (
-                <small className="text-red-600">
-                  {errors.password.message}
-                </small>
-              ) : (
-                ''
-              )}
-              <div className="flex items-center w-full p-2 my-2 mb-2 bg-gray-100 border rounded-md border-gray dark:bg-dark-bg ">
-                <FaRegEnvelope className="mr-2 text-gray-400" />
-                <input
-                  data-testid="email"
-                  type="email"
-                  {...register('email', { required: 'Email is required' })}
-                  placeholder={t('Email')}
-                  className="flex-1 px-2 text-sm bg-gray-100 outline-none dark:border-white dark:bg-dark-bg dark:text-white "
-                />
-              </div>
-              <div className="pl-4 mb-1 text-left">
-                {errors.email && (
-                  <small className="text-red-600">{errors.email.message}</small>
-                )}
-              </div>
+          <div className="w-[30vw] h-[42vh]  flex flex-row ">
+            <img
+              src={functionTree}
+              alt="functions"
+              className="w-sm dark:brightness-150"
+            />
+          </div>
+        </div>
+      </div>
 
-              <div className="flex items-center p-2 my-4 mb-2 bg-gray-100 border rounded-md md:w-full border-gray dark:border-white dark:bg-dark-bg">
-                <MdLockOutline className="mr-2 text-gray-400 " />
-                <input
-                  data-testid="password"
-                  type={passwordShown ? 'text' : 'password'}
-                  {...register('password', {
-                    required: 'Password is required',
-                  })}
-                  placeholder={t('Password')}
-                  className="flex-1 text-sm bg-gray-100 outline-none dark:border-white dark:bg-dark-bg dark:text-white"
-                />
-                <div className="text-gray-400 cursor-pointer onClick= {()=> handleShowPassword}">
-                  {passwordShown ? (
-                    <FaRegEye onClick={tooglePassword} />
-                  ) : (
-                    <FiEyeOff onClick={tooglePassword} />
-                  )}
-                </div>
-              </div>
-              <div className="pl-4 mb-1 text-left">
+      <div className=" w-full  lg:w-[50%] lg:p-5 flex flex-row items-end p-5">
+        <div className="max-w-lg w-full mx-auto my-28 bg-indigo-100 dark:bg-dark-bg  p-5 sm:p-5 md:shadow-xl sm:shadow-none md:rounded-xl sm:rounded-none">
+          <div className="">
+            <div className="flex flex-col justify-center items-center">
+              <h2 className="text-2xl font-bold text-primary dark:text-dark-text-fill ">
+                {t('Welcome to')}{' '}
+                {orgName
+                  ? orgName.charAt(0).toUpperCase() +
+                    orgName.slice(1).toLowerCase()
+                  : ''}
+              </h2>
+              <div className="border-[1px] w-10 bg-primary border-primary inline-block mb-2" />
+            </div>
+
+            <div className=" text-sm text-center dark:text-dark-text-fill">
+              <Link
+                to="/login/org"
+                className="mx-1 text-primary"
+                data-testid="switch"
+              >
+                {t('Switch')} {t('your organization')}
+              </Link>
+            </div>
+
+            <div className="flex flex-col items-center">
+              <form
+                action="#none"
+                className="w-full"
+                onSubmit={handleSubmit(onSubmit)}
+                data-testid="loginForm"
+              >
                 {errors.password &&
-                errors.password.message !== t('Invalid credentials') ? (
+                errors.password.message === t('Invalid credentials') ? (
                   <small className="text-red-600">
                     {errors.password.message}
                   </small>
                 ) : (
                   ''
                 )}
-              </div>
-              <div className="flex justify-between w-64 mt-5 mb-5 rounded">
-                <label
-                  htmlFor="checkbox"
-                  className="flex items-center text-xs dark:text-dark-text-fill "
-                >
+                <div className="flex items-center w-full p-2 my-2 mb-2 bg-gray-100 border rounded-md border-gray dark:bg-dark-bg ">
+                  <FaRegEnvelope className="mr-2 text-gray-400" />
                   <input
-                    type="checkbox"
-                    name="remember"
-                    className="mr-1 dark:text-dark-text-fill dark:border-white dark:bg-dark-frame-bg"
+                    data-testid="email"
+                    type="email"
+                    {...register('email', { required: 'Email is required' })}
+                    placeholder={t('Email')}
+                    className="bg-gray-100 outline-none text-sm flex-1 px-2 dark:border-white dark:bg-dark-bg dark:text-white "
                   />
-                  {t('Remember me')}
-                </label>
-                <Link
-                  to="/reset-password"
-                  className="text-xs dark:text-dark-text-fill "
-                >
-                  {t('Forgot Password?')}
-                </Link>
-              </div>
-              <div className="w-full justify-center ml-[-7px]">
-                {loading ? (
-                  <ButtonLoading style={'rounded-full inline-block'} />
-                ) : (
-                  <Button
-                    type="submit"
-                    variant="transparentbtn"
-                    size="md"
-                    style=" w-full bg-primary inline-block rounded-md lg:px-12 lg:py-2 sm:px-4 sm:py-1 md:font-semibold sm:mt-2 sm:font-medium text-white"
-                  >
-                    {t('Sign In')}
-                  </Button>
-                )}
-              </div>
-            </form>
-          </div>
+                </div>
+                <div className="text-left mb-1 pl-4">
+                  {errors.email && (
+                    <small className="text-red-600">
+                      {errors.email.message}
+                    </small>
+                  )}
+                </div>
 
-          <div className="my-4 text-sm text-center dark:text-dark-text-fill">
-            {t('First time here?')}
-            <Link to="/signup/org" className="mx-1 text-primary">
-              {t('Register')}
-            </Link>
-            {t('your organization')}
+                <div className="md:w-full border border-gray rounded-md bg-gray-100 p-2 my-4 flex items-center  mb-2 dark:border-white dark:bg-dark-bg">
+                  <MdLockOutline className="text-gray-400 mr-2 " />
+                  <input
+                    data-testid="password"
+                    type={passwordShown ? 'text' : 'password'}
+                    {...register('password', {
+                      required: 'Password is required',
+                    })}
+                    placeholder={t('Password')}
+                    className="bg-gray-100 outline-none text-sm flex-1 dark:border-white dark:bg-dark-bg dark:text-white"
+                  />
+                  <div className="text-gray-400 cursor-pointer onClick= {()=> handleShowPassword}">
+                    {passwordShown ? (
+                      <FaRegEye onClick={tooglePassword} />
+                    ) : (
+                      <FiEyeOff onClick={tooglePassword} />
+                    )}
+                  </div>
+                </div>
+                <div className="pl-4 mb-1 text-left">
+                  {errors.password &&
+                  errors.password.message !== t('Invalid credentials') ? (
+                    <small className="text-red-600">
+                      {errors.password.message}
+                    </small>
+                  ) : (
+                    ''
+                  )}
+                </div>
+                <div className="flex w-full flex-col sm:flex-row justify-between  items-center rounded mb-5 mt-5">
+                  <div className="w-50%">
+                    <label
+                      htmlFor="checkbox"
+                      className="flex items-center text-xs dark:text-dark-text-fill "
+                    >
+                      <input
+                        type="checkbox"
+                        name="remember"
+                        className="mr-1  dark:text-dark-text-fill dark:border-white dark:bg-dark-frame-bg"
+                      />
+                      {t('Remember me')}
+                    </label>
+                  </div>
+                  <div className="w-[50%] flex flex-row justify-end">
+                    <Link
+                      to="/reset-password"
+                      className="text-xs dark:text-dark-text-fill "
+                    >
+                      {t('Forgot Password?')}
+                    </Link>
+                  </div>
+                </div>
+                <div className="w-full justify-center ml-[-7px]">
+                  {loading ? (
+                    <ButtonLoading style={'rounded-full inline-block'} />
+                  ) : (
+                    <Button
+                      type="submit"
+                      variant="transparentbtn"
+                      size="md"
+                      style=" w-full bg-primary inline-block rounded-md lg:px-12 lg:py-2 sm:px-4 sm:py-1 md:font-semibold sm:mt-2 sm:font-medium text-white"
+                    >
+                      {t('Sign In')}
+                    </Button>
+                  )}
+                </div>
+              </form>
+            </div>
+
+            <div className="my-4 text-sm text-center dark:text-dark-text-fill">
+              {t('First time here?')}
+              <Link to="/signup/org" className="mx-1 text-primary">
+                {t('Register')}
+              </Link>
+              {t('your organization')}
+            </div>
           </div>
         </div>
       </div>
--- src/pages/Organization/Orglogin.tsx
+++ src/pages/Organization/Orglogin.tsx
@@ -1,13 +1,14 @@
 // no-use-before-define
-import React, { useState } from 'react';
+import React, { useId, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { useMutation } from '@apollo/client';
 import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router';
 import Button from '../../components/Buttons';
 import useDocumentTitle from '../../hook/useDocumentTitle';
+import functionTree from '../../assets/Functionality_Tree.svg';
+import pulseStars from '../../assets/Property 1=Logo_flie (1).svg';
 import LOGIN_ORGANIZATION_MUTATION from './LoginOrganisationMutation';
 import ButtonLoading from '../../components/ButtonLoading';
 import './orgName.css';
@@ -20,6 +21,7 @@ function Orglogin() {
 
   let isNameEmpty = true;
   const [inputWidth, setInputWidth] = useState('130px'); // Adjust the width as needed
+  const orgInputId = useId();
   // const [inputWidth, setInputWidth] = useState('133px'); // Adjust the width as needed
   const handleNameChange = async (e: { target: { value: any } }) => {
     const newName = e.target.value;
@@ -76,9 +78,8 @@ function Orglogin() {
         localStorage.setItem('orgToken', loginOrg.token);
         let value: string = String(name);
 
-        if (!value.includes('.devpulse.org')) {
-          value = `${name}.devpulse.org`;
-        }
+        value = `${name}`;
+
         // console.log(name)
         /* istanbul ignore next */
         // TODO:
@@ -105,101 +106,133 @@ function Orglogin() {
   const completeOrgUrl = `${names}`;
 
   return (
-    <div className="grow bg-neutral-100 dark:bg-dark-frame-bg flex flex-col justify-center font-sans">
-      <div className="max-w-lg w-full mx-auto my-28 bg-indigo-100 dark:bg-dark-bg p-14 md:shadow-xl sm:shadow-none md:rounded-xl sm:rounded-none">
-        <div className="text-center  text-black-600 text-2xl font-bold dark:text-dark-text-fill ">
-          {t('Sign in to your Organization')}
-        </div>
-        <div className="text-md  text-black-600 mt-2 text-center font-semibold dark:text-dark-text-fill sm:text-xs">
-          {t('Enter your organization’s Dev-Pulse URL')}
-        </div>
-        <form
-          action="#none"
-          className="space-y-6 mt-4 "
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className="orgName">
-            {!isNameEmpty ? (
-              <input
-                name="name"
-                placeholder="Your-organization"
-                type="text"
-                value={name}
-                data-testid="orgName"
-                // {...register('name', {
-                //   required: 'Organisation name is required',
-                // })}
-                onChange={handleNameChange}
-                onKeyDown={handleKeyPress}
-                style={{
-                  // paddingLeft: '25px',
-                  width: inputWidth,
-                  // backgroundColor: '  rgb(224 231 255 / var(--tw-bg-opacity)) ',
-                }} // Set the width dynamically
-                className="inputStyle inputOne w-full border-primary rounded mt-1 dark:bg-dark-bg"
-              />
-            ) : (
-              <input
-                name="name"
-                placeholder="Your-organization"
-                type="text"
-                value={name}
-                data-testid="orgName"
-                // {...register('name', {
-                //   required: 'Organisation name is required',
-                // })}
-                onChange={handleNameChange}
-                onKeyDown={handleKeyPress}
-                style={{
-                  // paddingLeft: '25px',
-                  width: inputWidth,
-                  // backgroundColor: ' rgb(24 231 255 / var(--tw-bg-opacity))',
-                }} // Set the width dynamically
-                className="inputStyle inputOne w-full border-primary rounded mt-1 dark:bg-neutral-600"
-              />
-            )}
-            <input
-              placeholder=".devpulse.org"
-              disabled
-              style={
-                {
-                  // width: inputWidth,
-                }
-              }
-              className="inputStyle inputTwo w-full p-2 border border-primary rounded mt-1 dark:bg-dark-bg"
+    <div className="grow  dark:bg-dark-frame-bg bg-indigo-50 flex flex-row font-sans">
+      <div className="hidden lg:flex w-[50%] h-full  flex-col justify-center items-center mt-10 ">
+        <div className=" flex flex-col justify-center items-center ">
+          <div className="relative flex flex-row justify-center">
+            <img
+              src={pulseStars}
+              alt="pulses"
+              className="absolute inset-0 w-[41rem] h-[12rem] dark:hidden"
             />
+            <p className="relative w-[70%] text-gray-700 text-[1.9em] p-5 text-center italic font-bold  dark:text-dark-text-fill">
+              {t('Boost your organization')}
+            </p>
           </div>
 
-          <div className="-mt-6">
-            {errors.name && (
-              <small className="text-red-600">{errors.name.message}</small>
-            )}
+          <div className="w-[30vw] h-[42vh]  flex flex-row ">
+            <img
+              src={functionTree}
+              alt="functions"
+              className="w-sm dark:brightness-150"
+            />
           </div>
-          <div>
-            {loading ? (
-              <ButtonLoading style="rounded-full inline-block" />
-            ) : (
-              <Button
-                type="submit"
-                variant="primary"
-                data-testid="loginForm"
-                size="lg"
-                style="w-full ml-0 hover:bg-violet-600 text-sm"
-              >
-                {' '}
-                {t('Continue')}
-              </Button>
-            )}
+        </div>
+      </div>
+
+      <div className=" w-full  lg:w-[50%] lg:p-5 flex flex-row items-end p-5">
+        <div className="max-w-lg w-full mx-auto my-28 bg-indigo-100 dark:bg-dark-bg  p-5 sm:p-14 md:shadow-xl sm:shadow-none md:rounded-xl sm:rounded-none">
+          <div className="text-center  text-black-600 text-2xl font-bold dark:text-dark-text-fill ">
+            {t('Sign in to your Organization')}
           </div>
-          <div className="w-full text-sm  text-light-text dark:text-dark-text-fill">
-            {t('Looking to register an organization instead?')}
-            <Link to="/signup/org">
-              <a href="#link" className="text-primary">
-                {t('Register a new organization')}
-              </a>
-            </Link>
+          <div className="text-md  text-black-600 mt-2 text-center font-semibold dark:text-dark-text-fill sm:text-xs">
+            {t('Enter your organization’s Dev-Pulse URL')}
           </div>
-        </form>
+          <form
+            action="#none"
+            className="space-y-6 mt-4"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <label
+              className="orgName bg-white dark:bg-neutral-600 rounded mt-1 py-1 px-2"
+              htmlFor={orgInputId}
+            >
+              {!isNameEmpty ? (
+                <input
+                  id={orgInputId}
+                  name="name"
+                  placeholder="Your-organization"
+                  type="text"
+                  value={name}
+                  data-testid="orgName"
+                  // {...register('name', {
+                  //   required: 'Organisation name is required',
+                  // })}
+                  onChange={handleNameChange}
+                  onKeyDown={handleKeyPress}
+                  style={{
+                    // paddingLeft: '25px',
+                    width: inputWidth,
+                    // backgroundColor: '  rgb(224 231 255 / var(--tw-bg-opacity)) ',
+                  }} // Set the width dynamically
+                  className="inputStyle inputOne w-full border-primary rounded  "
+                />
+              ) : (
+                <input
+                  id={orgInputId}
+                  name="name"
+                  placeholder="Your-organization"
+                  type="text"
+                  value={name}
+                  data-testid="orgName"
+                  // {...register('name', {
+                  //   required: 'Organisation name is required',
+                  // })}
+                  onChange={handleNameChange}
+                  onKeyDown={handleKeyPress}
+                  style={{
+                    // paddingLeft: '25px',
+                    width: inputWidth,
+                    // backgroundColor: ' rgb(24 231 255 / var(--tw-bg-opacity))',
+                  }} // Set the width dynamically
+                  className="inputStyle inputOne w-full border-primary rounded dark:bg-neutral-600"
+                />
+              )}
+              {/* <input
+                placeholder=".devpulse.org"
+                disabled
+                style={
+                  {
+                    // width: inputWidth,
+                  }
+                }
+                className="inputStyle inputTwo w-full p-2 border border-primary rounded mt-1 dark:bg-dark-bg"
+              /> */}
+              <span className="inputStyle inputTwo w-full p-2 border border-primary rounded text-gray-400">
+                .devpulse.org
+              </span>
+            </label>
+            <div className="-mt-6">
+              {errors.name && (
+                <small className="text-red-600">{errors.name.message}</small>
+              )}
+            </div>
+            <div>
+              {loading ? (
+                <ButtonLoading style="rounded-full inline-block" />
+              ) : (
+                <Button
+                  type="submit"
+                  variant="primary"
+                  data-testid="loginForm"
+                  size="lg"
+                  style="w-full ml-0 hover:bg-violet-600 text-sm"
+                >
+                  {' '}
+                  {t('Continue')}
+                </Button>
+              )}
+            </div>
+            <div className="w-full text-sm  text-light-text dark:text-dark-text-fill">
+              {t('Looking to register an organization instead?')}
+              <Link to="/signup/org">
+                <a href="#link" className="text-primary">
+                  {t('Register a new organization')}
+                </a>
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
