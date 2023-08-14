4c0fa66f2921265625e80a227487cc7434b8495d
Fidela1 <113624105+Fidela1@users.noreply.github.com>
2023-08-14 19:51:58 +0200

    mobile view of homepage (#257)
    Ft-create dashboard cards (#235)
    
    * fix login
    
    * create dashboard cards
    
    feat(cards)Dashboard cards:
    -design cards
    -add responsiveness to card
    -checkrole to access team cards
    
    feat(create):create dashboard cards
    -fetchdata with graphql
    
    -add team logo to card
    -get active trainees
    
    -assign appropriate color to each card
    -assign appropriate grade to each card
    -get quantity, quality and attendance to each team
    -add styling for color on card and appropriate arrow
    
    feat(create card)create dashboard cards
    -change create team to have all required input fiel
    -get ttl role for the team
    
    feat(manager-card)coordinator dashboardcard
    -remodeve managerEmail  field on add new team
    -displate coordinator on each team card
    -add appropriate message for manager or ttl if they are not yet added
    
    feat(dashoard)create dashboard card
    -add translation to team card
    
    create dashboard cards
    
    feat(create):create dashboard cards
    -fetchdata with graphql
    
    -assign appropriate color to each card
    -assign appropriate grade to each card
    -get quantity, quality and attendance to each team
    -add styling for color on card and appropriate arrow
    
    feat(create card)create dashboard cards
    -change create team to have all required input fiel
    -get ttl role for the team
    
    feat(manager-card)coordinator dashboardcard
    -remodeve managerEmail  field on add new team
    -displate coordinator on each team card
    -add appropriate message for manager or ttl if they are not yet added
    
    feat(dashoard)create dashboard card
    -add translation to team card
    
    ---------
    
    Co-authored-by: teerenzo <renzahoemmanuel8@gmail.com>
    Co-authored-by: Olivier <89464364+oliviertech212@users.noreply.github.com>
    
    CRUD on grading system (#241)
    
    ch(fix sign up):  fixing sign up (#236)
    
    - google is removed
    -Date of birth is validated to be at least 18 years
    -Email field is automatically filled and uneditable ( collect the email
    from the sign up link sent to them)
    -Password should not be too easy. (Should not be in the first 100 most
    common passwords: https://tinyurl.com/wiki-100)
    
    [Finishes #42-signup-fix]
    
     feat(orgName case insensitive): devpulse org login (#225) (#251)
    
    feature:redesigned help page (#250)
    
    * fix login
    
    * feat(helpPage):redesigned help page
    -change color theme
    -redesign tickets card
    
    ---------
    
    Co-authored-by: teerenzo <renzahoemmanuel8@gmail.com>
    
    mobile view of homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,32 @@ import { useTranslation } from 'react-i18next';
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
-import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 import { UserContext } from '../hook/useAuth';
 import useDocumentTitle from '../hook/useDocumentTitle';
-import Home from '../assets/Home.png'
-
 import Button from '../components/Buttons';
 import About from './About';
+
+import home from '../assets/Home_page_image.png'
+
+
+const backgroundImageStyle: React.CSSProperties = {
+  backgroundImage: `url(${home})`,
+  backgroundRepeat: 'no-repeat',
+  display: 'flex',
+  flexDirection: 'column',
+  flexGrow: 1,
+  height: '100vh',
+  width: '100%',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundAttachment: 'fixed',
+  // OBackgroundSize:'cover',
+  // MozBackgroundSize:'cover', 
+  // WebkitBackgroundSize:'cover', 
+
+  // WebkitTransform: 'translateZ(0)',
+};
 
 function LandingPage() {
   /* istanbul ignore next */
@@ -17,12 +36,11 @@ function LandingPage() {
   useDocumentTitle('Welcome');
   return (
     <div className="">                 
-      <Header styles="bg-opacity-50 dark:bg-opacity-50" />
-      <div className="relative">
-        <img src={Home} alt=""/>
-        <p className='text-img absolute bottom-1/3 left-44 right-0 px-4 py-2'>
+      <Navbar styles="bg-opacity-100 dark:bg-opacity-100" />
+    <div className='hero relative'>
+        <p className='text-img absolute bottom-1/3 lg:left-44 lg:right-0 lg:text-5xl sm:text-2xl px-4 py-2 md:left-0 md:right-0'>
          The brilliant performance management <br/> platform around the word.</p>
-         <div className='absolute items-center justify-start w-full text-2xl md:text-2xl lg:text-1xl md:mt-8 lg:mt-8 text-white top-2/3 right-96'>
+         <div className='absolute items-center justify-start w-full text-2xl md:text-2xl lg:text-1xl md:mt-8 lg:mt-8 text-white top-2/3 lg:right-96'>
          {!user?.auth ? (
             <div className=" w-full font absolute text-center justify-center items-center">
             <Link to="/signup/org">
