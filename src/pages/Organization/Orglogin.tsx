de9ca93970a850a03864c9e1259f5a15fa87da24
rachelumwari <umwarirachel13@gmail.com>
2023-08-16 17:10:05 +0200

    add changing

--- src/pages/Organization/Orglogin.tsx
+++ src/pages/Organization/Orglogin.tsx
@@ -18,17 +18,33 @@ function Orglogin() {
   const { t } = useTranslation();
   const [name, setName] = useState('');
 
-  const [inputWidth, setInputWidth] = useState('133px'); // Adjust the width as needed
+  let isNameEmpty = true;
+  const [inputWidth, setInputWidth] = useState('130px'); // Adjust the width as needed
   // const [inputWidth, setInputWidth] = useState('133px'); // Adjust the width as needed
   const handleNameChange = async (e: { target: { value: any } }) => {
     const newName = e.target.value;
     setName(newName);
+    if (newName === '') {
+      isNameEmpty = true;
+      setInputWidth(`130px`);
+    } else {
+      const newWidth = Math.max(newName.length * 11);
+
+      // console.log(newWidth)
+      setInputWidth(`${newWidth}px`);
+      isNameEmpty = false;
+    }
+    // console.log('1', name)
+
     // Calculate the width based on the length of the entered text
     // const newWidth = `${Math.max(newName.length * 9)}px`; // Adjust minimum width here
     // setInputWidth(newWidth);
-    const newWidth = Math.max(newName.length * 9);
-    if (newWidth > 120) setInputWidth(`${newWidth}px`);
+
+    // setInputWidth(`${newWidth}px`);
   };
+
+  // console.log('2', isNameEmpty);
+  // console.log('3', inputWidth)
 
   const formatVariable = async (e: any) => {
     const value: string = String(e.target.value);
@@ -63,7 +79,7 @@ function Orglogin() {
         if (!value.includes('.devpulse.org')) {
           value = `${name}.devpulse.org`;
         }
-        // console.log(value)
+        // console.log(name)
         /* istanbul ignore next */
         // TODO:
         localStorage.setItem('orgName', value);
@@ -85,7 +101,7 @@ function Orglogin() {
     }
   };
   const names = name.replace(/ /gi, '').toLowerCase();
-  // console.log('Bye Brother hey', names)
+  // console.log(names)
   const completeOrgUrl = `${names}`;
 
   return (
@@ -103,29 +119,53 @@ function Orglogin() {
           onSubmit={handleSubmit(onSubmit)}
         >
           <div className="orgName">
-            <input
-              name="name"
-              placeholder="Your-organization"
-              type="text"
-              value={name}
-              data-testid="orgName"
-              // {...register('name', {
-              //   required: 'Organisation name is required',
-              // })}
-              onChange={handleNameChange}
-              onKeyDown={handleKeyPress}
-              style={{
-                width: inputWidth,
-                backgroundColor: 'rgb(224 231 255 / var(--tw-bg-opacity))',
-              }} // Set the width dynamically
-              className="inputStyle inputOne w-full border-primary rounded mt-1 dark:bg-dark-bg"
-            />
+            {!isNameEmpty ? (
+              <input
+                name="name"
+                placeholder="Your-organization"
+                type="text"
+                value={name}
+                data-testid="orgName"
+                // {...register('name', {
+                //   required: 'Organisation name is required',
+                // })}
+                onChange={handleNameChange}
+                onKeyDown={handleKeyPress}
+                style={{
+                  // paddingLeft: '25px',
+                  width: inputWidth,
+                  backgroundColor: 'rgb(224 231 255 / var(--tw-bg-opacity))',
+                }} // Set the width dynamically
+                className="inputStyle inputOne w-full border-primary rounded mt-1 dark:bg-dark-bg"
+              />
+            ) : (
+              <input
+                name="name"
+                placeholder="Your-organization"
+                type="text"
+                value={name}
+                data-testid="orgName"
+                // {...register('name', {
+                //   required: 'Organisation name is required',
+                // })}
+                onChange={handleNameChange}
+                onKeyDown={handleKeyPress}
+                style={{
+                  // paddingLeft: '25px',
+                  width: inputWidth,
+                  backgroundColor: 'rgb(224 231 255 / var(--tw-bg-opacity))',
+                }} // Set the width dynamically
+                className="inputStyle inputOne w-full border-primary rounded mt-1 dark:bg-dark-bg"
+              />
+            )}
             <input
               placeholder=".devpulse.org"
               disabled
-              style={{
-                backgroundColor: 'rgb(224 231 255 / var(--tw-bg-opacity))',
-              }}
+              style={
+                {
+                  // width: inputWidth,
+                }
+              }
               className="inputStyle inputTwo w-full p-2 border border-primary rounded mt-1 dark:bg-dark-bg"
             />
           </div>
