67498fb65e6cc27e3a301b13eaa63a94f74b0347
Muhet <muhedarius96@gmail.com>
2023-11-13 10:43:39 +0100

    fixing cover image for profile

--- src/components/ProfileCoverpage.tsx
+++ src/components/ProfileCoverpage.tsx
@@ -62,7 +62,7 @@ export default function ProfileCoverpage({
       formData.append('upload_preset', 'my_upload');
 
       const avatar = await axios.post(
-        `${process.env.COVER_IMAGE_URL}`,
+        'https://api.cloudinary.com/v1_1/dj24yfas5/image/upload',
         formData,
       );
       /* istanbul ignore next */
@@ -97,7 +97,7 @@ export default function ProfileCoverpage({
       formData.append('upload_preset', 'my_upload');
 
       const cover = await axios.post(
-        `${process.env.COVER_IMAGE_URL}`,
+        'https://api.cloudinary.com/v1_1/dj24yfas5/image/upload',
         formData,
       );
       /* istanbul ignore next */
