4e5fc788099dc4a4ebfbf47e4ab6969530748ff3
Bryan Murasira <84919967+bryan936M@users.noreply.github.com>
2023-11-30 00:40:51 +0200

    Intergrating with the API (#348)

--- src/components/teamDetails.tsx
+++ src/components/teamDetails.tsx
@@ -423,8 +423,8 @@ function TeamDetails() {
                     <option value="1">Sprint 1</option>
                     <option value="2">Sprint 2</option>
                     <option value="3">Sprint 3</option>
-                    <option value="4">Sprint 4</option>
-                    <option value="5">Sprint 5</option>
+                    {/* <option value="4">Sprint 4</option>
+                    <option value="5">Sprint 5</option> */}
                   </select>
                 </div>
               </div>
