a1e63ad42664c7beea5009bbb7f0002941111a8b
Sophie Mukamugema <sofidele12@.com>
2024-09-03 19:46:03 +0200

    end--no-edit

--- src/components/InvitationCard.tsx
+++ src/components/InvitationCard.tsx
@@ -13,21 +13,23 @@ interface InvitationCardType {
 // Define the InvitationCard component using a function declaration
 function InvitationCard({ icon, status, time, staticNumber, percentage }: InvitationCardType) {
   return (
-    <div className='bg-blue-100 w-48 grid grid-row-4 rounded-xl h-40 text-black '>
-      <div>
-        <h3 className='ml-6 text-xl font-serif font-bold text-[18px] mt-4 '>{status}</h3>
-        <div className='flex w-8 h-10 ml-2 -mt-2 '>
-          {icon}
+    <div className='bg-blue-100 w-48 grid grid-row-4 h-40 text-black '>
+      
+        <div className='flex flex-row mt-4 gap-2' >
+          <div className='flex w-8 h-10 '>
+            {icon}
+          </div>
+          <h3 className='text-xl font-serif font-bold text-[12px] '>{status}</h3>
         </div>
         <div>
-          <p className='ml-10 -mt-2'>{time}</p>
+          <p className='-mt-12 ml-12 text-[15px] text-gray-500'>{time}</p>
         </div>
-      </div>
-      <div className='flex justify-center items-center -mt-8'>
-        <div className='text-2xl font-serif font-bold text-[40px]'>{staticNumber}
+      
+      <div className='flex justify-center items-center -mt-9 '>
+        <div className='text-xl font-serif font-bold mb-4 text-[50px]'>{staticNumber}
         </div>
-        <div className='text-base text-[#7258ce] ml-3 mb-0'>
-          <AiOutlineRise className="text-2xl" />
+        <div className='text-base text-[#7258ce] ml-3 mb-2'>
+          <AiOutlineRise className="text-xl" />
           <span>{percentage}</span>
         </div>
       </div>
