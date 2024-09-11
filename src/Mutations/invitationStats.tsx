c65e379bc1c7c0676a19e61dcd8b7d39d405bc85
Grace UWICYEZA <124083824+UwicyezaG@users.noreply.github.com>
2024-09-11 18:13:57 +0200

    Fetch invitation statistics (#463)

---
+++ src/Mutations/invitationStats.tsx
@@ -0,0 +1,12 @@
+import { gql } from '@apollo/client';
+export const GET_INVITATIONS_STATISTICS_QUERY = gql`
+  query GetInvitationStatistics($orgToken: String!){
+    getInvitationStatistics(orgToken: $orgToken){
+      totalInvitations
+      pendingInvitationsCount
+      getPendingInvitationsPercentsCount
+      getAcceptedInvitationsPercentsCount
+      acceptedInvitationsCount
+    }
+  }
+`;
