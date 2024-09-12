a7719409c0b7b5a4ef632e668390f6a46f3af4e0
IRADUKUNDA SANGWA CEDRIC <110623461+Dawaic6@users.noreply.github.com>
2024-09-12 16:29:09 +0200

    fix organization not found

--- src/Mutations/invitationStats.tsx
+++ src/Mutations/invitationStats.tsx
@@ -1,7 +1,8 @@
 import { gql } from '@apollo/client';
+
 export const GET_INVITATIONS_STATISTICS_QUERY = gql`
-  query GetInvitationStatistics($orgToken: String!){
-    getInvitationStatistics(orgToken: $orgToken){
+  query GetInvitationStatistics($orgToken: String!) {
+    getInvitationStatistics(orgToken: $orgToken) {
       totalInvitations
       pendingInvitationsCount
       getPendingInvitationsPercentsCount
