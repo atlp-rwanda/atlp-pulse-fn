d24d6f8a083131aeb9a69dacb56015789f316604
Yvette IZANYIBUKA <107992107+YvetteNyibuka@users.noreply.github.com>
2024-09-13 13:24:51 +0200

    feat(Filter Invitations statistics): Filter statistics based on time range (#488)
    Co-authored-by: YvetteNyibuka <izanybukayvette@gmail.com>

--- src/Mutations/invitationStats.tsx
+++ src/Mutations/invitationStats.tsx
@@ -1,7 +1,18 @@
 import { gql } from '@apollo/client';
+
 export const GET_INVITATIONS_STATISTICS_QUERY = gql`
-  query GetInvitationStatistics($orgToken: String!){
-    getInvitationStatistics(orgToken: $orgToken){
+  query GetInvitationStatistics(
+    $orgToken: String!
+    $startDate: String
+    $endDate: String
+    $daysRange: Int
+  ) {
+    getInvitationStatistics(
+      orgToken: $orgToken
+      startDate: $startDate
+      endDate: $endDate
+      daysRange: $daysRange
+    ) {
       totalInvitations
       pendingInvitationsCount
       getPendingInvitationsPercentsCount
--- src/pages/invitation.tsx
+++ src/pages/invitation.tsx
@@ -4,11 +4,12 @@ import { IoIosAddCircleOutline, IoIosSearch } from 'react-icons/io';
 import { FaCheck, FaFilter } from 'react-icons/fa';
 import { LuHourglass } from 'react-icons/lu';
 import { BsPersonFillX } from 'react-icons/bs';
+import { toast } from 'react-toastify';
 import InvitationCard from '../components/InvitationCard';
 import InvitationTable from '../components/InvitationTable';
 import InvitationModal from './invitationModalComponet';
 import { GET_INVITATIONS_STATISTICS_QUERY } from '../Mutations/invitationStats';
-import { toast } from 'react-toastify';
+import InvitationCardSkeleton from '../Skeletons/InvitationCardSkeleton';
 
 const GET_ALL_INVITATIONS = gql`
   query AllInvitations($limit: Int, $offset: Int) {
@@ -62,30 +63,68 @@ function Invitation() {
   const [pageIndex, setPageIndex] = useState<number>(0);
   const [pageSize, setPageSize] = useState<number>(5);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [filterLoading, setFilterLoading] = useState<boolean>(true);
+  const [filterRange, setFilterRange] = useState<string>('Last 7 days');
+  const [customRange, setCustomRange] = useState({
+    startDate: '',
+    endDate: '',
+  });
   const organizationToken = localStorage.getItem('orgToken');
-  
-      const { loading: isLoading, data: queryData, refetch: refreshData } = useQuery(
-        GET_INVITATIONS_STATISTICS_QUERY,
-        {
-          variables: {
-            orgToken: organizationToken
-          },
-          skip: !organizationToken,
-          fetchPolicy: 'network-only',
-          onError: (error) => {
-            toast.error("testtes111");
-          },
-        },
-      );
-      useEffect(() => {
-        if (queryData) {
-            refreshData();
-          setInvitationStats(queryData.getInvitationStatistics);
-        }
-      }, [queryData, refreshData]);
-      if (!organizationToken) {
-        return <p>Organization token not found. Please log in.</p>;
-      }
+
+  const parseRange = (range: string) => {
+    switch (range) {
+      case 'Last 7 days':
+        return 7;
+      case 'Last 30 days':
+        return 30;
+      case 'Last 90 days':
+        return 90;
+      default:
+        return null;
+    }
+  };
+
+  const {
+    loading: isLoading,
+    data: queryData,
+    refetch: refreshData,
+  } = useQuery(GET_INVITATIONS_STATISTICS_QUERY, {
+    variables: {
+      orgToken: organizationToken,
+      daysRange:
+        filterRange !== 'Custom range' ? parseRange(filterRange) : null,
+      startDate: filterRange === 'Custom range' ? customRange.startDate : null,
+      endDate: filterRange === 'Custom range' ? customRange.endDate : null,
+    },
+    skip: !organizationToken,
+    fetchPolicy: 'network-only',
+    onError: (error) => {
+      toast.error('testtes111');
+    },
+  });
+  useEffect(() => {
+    if (isLoading) {
+      setFilterLoading(true);
+    } else {
+      setFilterLoading(false);
+    }
+    if (queryData) {
+      refreshData();
+      setInvitationStats(queryData.getInvitationStatistics);
+    }
+  }, [queryData, refreshData]);
+
+  const handleRangeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterRange(e.target.value);
+  };
+
+  const handleCustomRangeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setCustomRange((prev) => ({ ...prev, [name]: value }));
+  };
+  if (!organizationToken) {
+    return <p>Organization token not found. Please log in.</p>;
+  }
 
   const {
     data,
@@ -202,81 +241,147 @@ function Invitation() {
     <div className="w-full">
       {/* Header and Invite Button */}
       <div className="flex flex-row sm:flex-rowmd:flex-row md:justify-between items-center md:gap-4 mb-8 ">
-        <div className='flex w-full items-center justify-between'>
-          <h1 className='font-bold lg:text-xl sm:text-lg xm:text-lg w-full'>Invitation Stats</h1>
-        <div className="flex items-center rounded-md shadow-sm bg-white dark:bg-[#020917] border-2 border-black dark:border-white h-8 w-[40%] md:hidden ml-5">
-          <FaFilter className="text-[#9e85f5] ml-1" />
-            {/* Select Dropdown for Small Screens */}
-          <select className="bg-transparent dark:bg-[#020917] text-gray-700 dark:text-white outline-none text-sm py-1">
-            <option value="Select range">Select range</option>
-            <option value="Last 7 days">Last 7 days</option>
-            <option value="Last 30 days">Last 30 days</option>
-            <option value="Last 90 days">Last 90 days</option>
-            <option value="Custom range">Custom range</option>
-          </select>
-        </div>
-        <button
-          type="button"
-          onClick={handleOpenModal}
-          className="lg:bg-[#9e85f5] bg-none lg:text-white text-[#9e85f5] text-lg lg:text-lg rounded-md h-10 flex items-center justify-center w-[15%]"
-        >
-          <IoIosAddCircleOutline className="w-6 h-6 md:w-8 md:h-8 md:mr-2 xm:w-8 xm:h-8 sm:w-10 sm:h-10" />
-          <span className="hidden lg:block">Invite User</span>
-        </button>
+        <div className="flex w-full items-center justify-between">
+          <h1 className="font-bold lg:text-xl sm:text-lg xm:text-lg w-full">
+            Invitation Stats
+          </h1>
+          <div className="flex flex-col  w-full p-3 items-start">
+            <div className="flex w-full items-center rounded-md shadow-sm bg-white dark:bg-[#020917] border-2 border-black dark:border-white h-8  md:hidden ">
+              <FaFilter className="text-[#9e85f5] ml-1" />
+              {/* Select Dropdown for Small Screens */}
+              <select
+                value={filterRange}
+                onChange={handleRangeChange}
+                className="bg-transparent dark:bg-[#020917] text-gray-700 dark:text-white outline-none text-sm py-1"
+              >
+                <option value="Select range">Select range</option>
+                <option value="Last 7 days">Last 7 days</option>
+                <option value="Last 30 days">Last 30 days</option>
+                <option value="Last 90 days">Last 90 days</option>
+                <option value="Custom range">Custom range</option>
+              </select>
+            </div>
+            {filterRange === 'Custom range' && (
+              <div className="flex  flex-col items-start gap-1 mt-2 md:hidden">
+                {/* <label htmlFor="from">From:</label> */}
+                <input
+                  type="date"
+                  name="startDate"
+                  value={customRange.startDate}
+                  onChange={handleCustomRangeChange}
+                  className="border border-gray-300 rounded-md px-3 py-1 h-10 dark:bg-dark-bg dark:text:text-white"
+                />
+                {/* <label htmlFor="to">To:</label> */}
+                <input
+                  type="date"
+                  name="endDate"
+                  value={customRange.endDate}
+                  onChange={handleCustomRangeChange}
+                  className="border border-gray-300 rounded-md px-3 py-1 h-10 dark:bg-dark-bg dark:text:text-white"
+                />
+              </div>
+            )}
+          </div>
+          <button
+            type="button"
+            onClick={handleOpenModal}
+            className="lg:bg-[#9e85f5] bg-none lg:text-white text-[#9e85f5] text-lg lg:text-lg rounded-md h-10 flex items-center justify-center w-[27%]"
+          >
+            <IoIosAddCircleOutline className="w-6 h-6 md:w-8 md:h-8 md:mr-2 xm:w-8 xm:h-8 sm:w-10 sm:h-10" />
+            <span className="hidden lg:block">Invite User</span>
+          </button>
         </div>
       </div>
       <InvitationModal isOpen={isModalOpen} onClose={handleCloseModal} />
       {/* Container for Select and Cards */}
       <div className="flex flex-col border-blue-500 md:flex-row gap-4 md:gap-5 mb-10">
-
         {/* Invitation Cards */}
+
         <div className="grid grid-row-2 gap-4 sm:gap-4 mb-4 w-full lg:flex sm:grid grid-cols-2">
-          <InvitationCard
-            icon={
-              <FaCheck className="text-[#9e85f5] w-8 h-8 md:w-12 md:h-12 " />
-            }
-            status="ACCEPTED"
-            time="Last 7 days"
-            staticNumber={invitationStats?.acceptedInvitationsCount || 0}
-            percentage={
-                invitationStats?.getAcceptedInvitationsPercentsCount?.toFixed(
+          {filterLoading ? (
+            <>
+              <InvitationCardSkeleton />
+              <InvitationCardSkeleton />
+              <InvitationCardSkeleton />
+            </>
+          ) : (
+            <>
+              <InvitationCard
+                icon={
+                  <FaCheck className="text-[#9e85f5] w-8 h-8 md:w-12 md:h-12 " />
+                }
+                status="ACCEPTED"
+                time="Last 7 days"
+                staticNumber={invitationStats?.acceptedInvitationsCount || 0}
+                percentage={
+                  `${invitationStats?.getAcceptedInvitationsPercentsCount?.toFixed(
                     1,
-                 ) + '%' || '0'
-                        }
-          />
-          <InvitationCard
-            icon={
-              <LuHourglass className="text-[#9e85f5] w-8 h-8 md:w-12 md:h-12" />
-            }
-            status="PENDING"
-            time="Last 7 days"
-            staticNumber={invitationStats?.pendingInvitationsCount || 0}
-            percentage={
-                invitationStats?.getPendingInvitationsPercentsCount?.toFixed(
-                     1,
-                ) + '%' || '0' 
-                        }
-          />
-         
-          <InvitationCard
-            icon=""
-            status="INVITATIONS"
-            time="Last 7 days"
-            staticNumber={invitationStats?.totalInvitations || 0}
-            percentage="100%"
-          />
+                  )}%` || '0'
+                }
+              />
+              <InvitationCard
+                icon={
+                  <LuHourglass className="text-[#9e85f5] w-8 h-8 md:w-12 md:h-12" />
+                }
+                status="PENDING"
+                time="Last 7 days"
+                staticNumber={invitationStats?.pendingInvitationsCount || 0}
+                percentage={
+                  `${invitationStats?.getPendingInvitationsPercentsCount?.toFixed(
+                    1,
+                  )}%` || '0'
+                }
+              />
+
+              <InvitationCard
+                icon=""
+                status="INVITATIONS"
+                time="Last 7 days"
+                staticNumber={invitationStats?.totalInvitations || 0}
+                percentage={
+                  invitationStats?.totalInvitations === 0 ? '0%' : '100%'
+                }
+              />
+            </>
+          )}
         </div>
 
-        {/* Select Dropdown for Desktop */}
-        <div className=".flex  items-center rounded-md shadow-sm px-3 py-2 bg-white dark:bg-[#020917] border-2 border-black dark:border-white space-x-2 h-8 hidden md:flex md:ml-8 ">
-          <FaFilter className="text-[#9e85f5]" />
-          <select className="bg-transparent dark:bg-[#020917] text-gray-700 dark:text-white outline-none ">
-            <option value="Select range">Select range</option>
-            <option value="Last 7 days">Last 7 days</option>
-            <option value="Last 30 days">Last 30 days</option>
-            <option value="Last 90 days">Last 90 days</option>
-            <option value="Custom range">Custom range</option>
-          </select>
+        <div className="flex flex-col  w-fit p-3 items-start">
+          {/* Select Dropdown for Desktop */}
+          <div className=".flex  items-center rounded-md shadow-sm px-3 py-2 bg-white dark:bg-[#020917] border-2 border-black dark:border-white space-x-2 h-8 hidden md:flex ">
+            <FaFilter className="text-[#9e85f5]" />
+            <select
+              value={filterRange}
+              onChange={handleRangeChange}
+              className="bg-transparent dark:bg-[#020917] text-gray-700 dark:text-white outline-none "
+            >
+              <option value="Select range">Select range</option>
+              <option value="Last 7 days">Last 7 days</option>
+              <option value="Last 30 days">Last 30 days</option>
+              <option value="Last 90 days">Last 90 days</option>
+              <option value="Custom range">Custom range</option>
+            </select>
+          </div>
+          {filterRange === 'Custom range' && (
+            <div className="md:flex  flex-col items-start gap-1 hidden ">
+              <label htmlFor="from">From:</label>
+              <input
+                type="date"
+                name="startDate"
+                value={customRange.startDate}
+                onChange={handleCustomRangeChange}
+                className="border border-gray-300 rounded-md px-3 py-2 h-10 dark:bg-dark-bg dark:text:text-white"
+              />
+              <label htmlFor="to">To:</label>
+              <input
+                type="date"
+                name="endDate"
+                value={customRange.endDate}
+                onChange={handleCustomRangeChange}
+                className="border border-gray-300 rounded-md px-3 py-2 h-10 dark:bg-dark-bg dark:text:text-white"
+              />
+            </div>
+          )}
         </div>
       </div>
 
@@ -326,4 +431,3 @@ function Invitation() {
 }
 
 export default Invitation;
-
