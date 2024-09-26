8192375c12a1601943b7ecbd5992f510d047608a
Fred Shema <51389166+fredshema@users.noreply.github.com>
2024-09-26 10:46:52 +0200

    ch: add redirect handler (#508)
    fix: linting errors
    
    fix: fallback handler

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-  plugins: [tailwindcss('./tailwind.config.js'), autoprefixer],
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
