c0bcec63c73eeaeeee258f5c7b534048a23e539a
kanu-cast <karkay349@gmail.com>
2024-10-04 13:31:24 +0200

    updating-trainee-dashboard

--- src/pages/TraineeDashboard.tsx
+++ src/pages/TraineeDashboard.tsx
@@ -296,212 +296,225 @@ function SupAdDashboard() {
   };
 
   return (
-    <div className="flex flex-col  grow dark:bg-dark-frame-bg font-serif">
-      <div className="flex  flex-col justify-center  ">
-        <div className="w-[100%] pt-[4vh] h-[100%]   ">
-          <div className="">
-            <div className="flex bg-[#b8cdba] w-[30%] h-[48px]  ml-7 items-center justify-center  rounded-lg cohort-phase dark:bg-[#8667F2]">
-              <span className="flex  bg-[#b8cdba] pl-2 items-center rounded-md font-semibold text-xl w-[30%] dark:bg-[#8667F2]">
-                {cohort}
-              </span>
-              <span className="h-5 border-2 mx-1 text-xl cohort-bar"></span>
-
-              <span className="w-[45%] font-semibold text-xl pr-1 pl-3 c-phase ">
-                {rating && (
-                  <select
-                    className="flex items-center bg-[#b8cdba] rounded-md font-semibold text-xl cursor-pointer phasesss dark:bg-[#8667F2]"
-                    onChange={(e) => setPhase(e.target.value)}
-                    value={phase}
-                  >
-                    {phases.map((phase: any, index) => (
-                      <option key={index} value={phase}>
-                        {phase}
-                      </option>
-                    ))}
-                  </select>
-                )}
-              </span>
+    <div className="flex flex-col  grow dark:bg-dark-frame-bg font-serif" style={{ minHeight: '80vh' }}>
+      <div className="flex flex-col justify-center items-center flex-grow" style={{ flexGrow: 1 }}>
+        {norating ? 
+          <div className="containered flex items-center justify-center w-full h-full ">
+            <div className="centered d-block text-center justify-center">
+              <img src='https://res.cloudinary.com/dmems8p0a/image/upload/v1727875739/brizjlb7mkhekbxgcsdz.png'
+                style={{height:'180px', width:'180',margin:'auto'}}
+                alt='not found'
+              />
+              <span className='text-muted text-lg font-bold text-red-700'>You have no ratings yet...</span><br/>
+              <span className='text-muted text-sm'>Once you receive ratings they will be displayed here</span>
             </div>
           </div>
+        :
+          <div className="w-[100%] pt-[4vh] h-[100%]   ">
+            <div className="">
+              <div className="flex bg-[#b8cdba] w-[30%] h-[48px]  ml-7 items-center justify-center  rounded-lg cohort-phase dark:bg-[#8667F2]">
+                <span className="flex  bg-[#b8cdba] pl-2 items-center rounded-md font-semibold text-xl w-[30%] dark:bg-[#8667F2]">
+                  {cohort}
+                </span>
+                <span className="h-5 border-2 mx-1 text-xl cohort-bar"></span>
 
-          <p className=" font-bold ml-7 my-10 text-2xl"> Perfomance score</p>
-          <div className=" flex flex-row ">
-            <div className=" Progres-bar flex flex-row w-[90%]  justify-between ml-3  ">
-              <div className="  flex flex-col w-[30%] ml-3   lg:flex-row perfomance-bar">
-                <div className=" w-[130px]">
-                  <CircularProgressbar
-                    value={(quantityValue / 2) * 100}
-                    text={`${quantityValue}`}
-                    styles={getProgressBarStyles(quantityValue)}
-                  />
-                </div>
-
-                <div className=" flex flex-col justify-center ml-3">
-                  <ul className="list-disc ml-4">
-                    <li>Quantity</li>
-                  </ul>
-                  <p
-                    className={`ml-2 ${
-                      quantityValue > 1
-                        ? 'text-[#1b5e20]'
-                        : quantityValue === 1
-                        ? 'text-[#ffeb3b]'
-                        : 'text-[#b71c1c]'
-                    }`}
-                  >
-                    {norating ? (
-                      <h1>No ratings yet </h1>
-                    ) : quantityValue >= 1.5 ? (
-                      <h1>Very Good</h1>
-                    ) : quantityValue >= 1 ? (
-                      <h1>Good</h1>
-                    ) : (
-                      <h1>Need to improve</h1>
-                    )}
-                  </p>
-                </div>
-              </div>
-              <div className="flex flex-col w-[30%]  flex-wrap   lg:flex-row quality-bar ">
-                <div className=" w-[130px]">
-                  <CircularProgressbar
-                    value={(qualityValue / 2) * 100}
-                    text={`${qualityValue}`}
-                    styles={getProgressBarStyles(qualityValue)}
-                  />
-                </div>
-
-                <div className=" flex flex-col justify-center ml-3">
-                  <ul className="list-disc ml-4">
-                    <li>Quality</li>
-                  </ul>
-                  <p
-                    className={`ml-2 ${
-                      qualityValue > 1
-                        ? 'text-[#1b5e20]'
-                        : qualityValue === 1
-                        ? 'text-[#ffeb3b]'
-                        : 'text-[#b71c1c]'
-                    }`}
-                  >
-                    {norating ? (
-                      <h1>No ratings yet </h1>
-                    ) : qualityValue >= 1.5 ? (
-                      <h1>Very Good</h1>
-                    ) : qualityValue >= 1 ? (
-                      <h1>Good</h1>
-                    ) : (
-                      <h1>Need to improve</h1>
-                    )}
-                  </p>
-                </div>
-              </div>
-              <div className="flex flex-col w-[30%] flex-wrap  lg:flex-row quantity-bar ">
-                <div className=" w-[130px]">
-                  <CircularProgressbar
-                    value={(perfomanceValue / 2) * 100}
-                    text={`${perfomanceValue}`}
-                    styles={getProgressBarStyles(perfomanceValue)}
-                  />
-                </div>
-
-                <div className=" flex flex-col justify-center ml-3">
-                  <ul className="list-disc ml-4">
-                    <li>Professionalism</li>
-                  </ul>
-                  <p
-                    className={`ml-2 ${
-                      perfomanceValue > 1
-                        ? 'text-[#1b5e20]'
-                        : perfomanceValue === 1
-                        ? 'text-[#ffeb3b]'
-                        : 'text-[#b71c1c]'
-                    }`}
-                  >
-                    {norating ? (
-                      <h1> No ratings yet</h1>
-                    ) : perfomanceValue >= 1.5 ? (
-                      <h1>Very Good</h1>
-                    ) : perfomanceValue >= 1 ? (
-                      <h1>Good</h1>
-                    ) : (
-                      <h1>Need to improve</h1>
-                    )}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <p className=" font-bold ml-7 my-10 text-2xl"> Stats</p>
-
-          <div className=" w-[100%] ml-7">
-            <TraineeChart barChartData={chartData} />
-          </div>
-          <div className="p-4">
-            <h1 className="text-2xl font-bold mb-4 ">Recents feedback</h1>
-            <div className="p-2 grid grid-cols-2 phases ">
-              <div className="flex  items-center ">
-                {phases.map((phase: any, index) => (
-                  <>
-                    <span
-                      key={index}
-                      className="mr-2 font-bold "
-                      style={{
-                        cursor: 'pointer',
-                        fontFamily: 'inter',
-                        fontSize: '14px',
-                        color: '#b1b1b1',
-                        borderBottom: '3px solid #6b6319',
-                        borderRadius: '1px',
-                      }}
-                      onClick={() => setPhase(phase)}
+                <span className="w-[45%] font-semibold text-xl pr-1 pl-3 c-phase ">
+                  {rating && (
+                    <select
+                      className="flex items-center bg-[#b8cdba] rounded-md font-semibold text-xl cursor-pointer phasesss dark:bg-[#8667F2]"
+                      onChange={(e) => setPhase(e.target.value)}
+                      value={phase}
                     >
-                      {phase}
-                    </span>
-                    <div className="h-5 border-2 bg-[#b1b1b1] mx-0" />
-                  </>
-                ))}
+                      {phases.map((phase: any, index) => (
+                        <option key={index} value={phase}>
+                          {phase}
+                        </option>
+                      ))}
+                    </select>
+                  )}
+                </span>
               </div>
             </div>
-            <div className="trainee-table">
-              <div className="font-bold my-10 w-[100%] text-lg ">
-                {chartData.length !== 0 ? (
-                  <DataTable
-                    columns={columns}
-                    data={chartData ? (chartData as [any]) : []}
-                    title={t('Sprint ratings')}
-                    loading={loading}
-                  />
-                ) : (
-                  <table className="min-w-full leading-normal">
-                    <thead>
-                      <tr>
-                        {columns.map((column, columnIndex) => (
-                          <th
-                            key={columnIndex}
-                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:bg-neutral-600 uppercase tracking-wider"
+
+            <p className=" font-bold ml-7 my-10 text-2xl"> Perfomance score</p>
+            <div className=" flex flex-row ">
+              <div className=" Progres-bar flex flex-row w-[90%]  justify-between ml-3  ">
+                <div className="  flex flex-col w-[30%] ml-3   lg:flex-row perfomance-bar">
+                  <div className=" w-[130px]">
+                    <CircularProgressbar
+                      value={(quantityValue / 2) * 100}
+                      text={`${quantityValue}`}
+                      styles={getProgressBarStyles(quantityValue)}
+                    />
+                  </div>
+
+                  <div className=" flex flex-col justify-center ml-3">
+                    <ul className="list-disc ml-4">
+                      <li>Quantity</li>
+                    </ul>
+                    <p
+                      className={`ml-2 ${
+                        quantityValue > 1
+                          ? 'text-[#1b5e20]'
+                          : quantityValue === 1
+                          ? 'text-[#ffeb3b]'
+                          : 'text-[#b71c1c]'
+                      }`}
+                    >
+                      {norating ? (
+                        <h1>No ratings yet </h1>
+                      ) : quantityValue >= 1.5 ? (
+                        <h1>Very Good</h1>
+                      ) : quantityValue >= 1 ? (
+                        <h1>Good</h1>
+                      ) : (
+                        <h1>Need to improve</h1>
+                      )}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col w-[30%]  flex-wrap   lg:flex-row quality-bar ">
+                  <div className=" w-[130px]">
+                    <CircularProgressbar
+                      value={(qualityValue / 2) * 100}
+                      text={`${qualityValue}`}
+                      styles={getProgressBarStyles(qualityValue)}
+                    />
+                  </div>
+
+                  <div className=" flex flex-col justify-center ml-3">
+                    <ul className="list-disc ml-4">
+                      <li>Quality</li>
+                    </ul>
+                    <p
+                      className={`ml-2 ${
+                        qualityValue > 1
+                          ? 'text-[#1b5e20]'
+                          : qualityValue === 1
+                          ? 'text-[#ffeb3b]'
+                          : 'text-[#b71c1c]'
+                      }`}
+                    >
+                      {norating ? (
+                        <h1>No ratings yet </h1>
+                      ) : qualityValue >= 1.5 ? (
+                        <h1>Very Good</h1>
+                      ) : qualityValue >= 1 ? (
+                        <h1>Good</h1>
+                      ) : (
+                        <h1>Need to improve</h1>
+                      )}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col w-[30%] flex-wrap  lg:flex-row quantity-bar ">
+                  <div className=" w-[130px]">
+                    <CircularProgressbar
+                      value={(perfomanceValue / 2) * 100}
+                      text={`${perfomanceValue}`}
+                      styles={getProgressBarStyles(perfomanceValue)}
+                    />
+                  </div>
+
+                  <div className=" flex flex-col justify-center ml-3">
+                    <ul className="list-disc ml-4">
+                      <li>Professionalism</li>
+                    </ul>
+                    <p
+                      className={`ml-2 ${
+                        perfomanceValue > 1
+                          ? 'text-[#1b5e20]'
+                          : perfomanceValue === 1
+                          ? 'text-[#ffeb3b]'
+                          : 'text-[#b71c1c]'
+                      }`}
+                    >
+                      {norating ? (
+                        <h1> No ratings yet</h1>
+                      ) : perfomanceValue >= 1.5 ? (
+                        <h1>Very Good</h1>
+                      ) : perfomanceValue >= 1 ? (
+                        <h1>Good</h1>
+                      ) : (
+                        <h1>Need to improve</h1>
+                      )}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <p className=" font-bold ml-7 my-10 text-2xl"> Stats</p>
+
+            <div className=" w-[100%] ml-7">
+              <TraineeChart barChartData={chartData} />
+            </div>
+            <div className="p-4">
+              <h1 className="text-2xl font-bold mb-4 ">Recents feedback</h1>
+              <div className="p-2 grid grid-cols-2 phases ">
+                <div className="flex  items-center ">
+                  {phases.map((phase: any, index) => (
+                    <>
+                      <span
+                        key={index}
+                        className="mr-2 font-bold "
+                        style={{
+                          cursor: 'pointer',
+                          fontFamily: 'inter',
+                          fontSize: '14px',
+                          color: '#b1b1b1',
+                          borderBottom: '3px solid #6b6319',
+                          borderRadius: '1px',
+                        }}
+                        onClick={() => setPhase(phase)}
+                      >
+                        {phase}
+                      </span>
+                      <div className="h-5 border-2 bg-[#b1b1b1] mx-0" />
+                    </>
+                  ))}
+                </div>
+              </div>
+              <div className="trainee-table">
+                <div className="font-bold my-10 w-[100%] text-lg ">
+                  {chartData.length !== 0 ? (
+                    <DataTable
+                      columns={columns}
+                      data={chartData ? (chartData as [any]) : []}
+                      title={t('Sprint ratings')}
+                      loading={loading}
+                    />
+                  ) : (
+                    <table className="min-w-full leading-normal">
+                      <thead>
+                        <tr>
+                          {columns.map((column, columnIndex) => (
+                            <th
+                              key={columnIndex}
+                              className="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:bg-neutral-600 uppercase tracking-wider"
+                            >
+                              {column.Header}
+                            </th>
+                          ))}
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td
+                            colSpan={columns.length}
+                            className="px-6 py-4 whitespace-nowrap text-center text-gray-500"
                           >
-                            {column.Header}
-                          </th>
-                        ))}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td
-                          colSpan={columns.length}
-                          className="px-6 py-4 whitespace-nowrap text-center text-gray-500"
-                        >
-                          {t('No ratings  yet')}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                )}
+                            {t('No ratings  yet')}
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  )}
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        }
       </div>
     </div>
   );
