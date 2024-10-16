012f02450ff384c96b99d422628001a9045a6155
aimedivin <aimedifi003@gmail.com>
2024-10-16 06:52:15 +0200

    fix: move client-side attendance logic to server

--- src/components/AttendanceSymbols.tsx
+++ src/components/AttendanceSymbols.tsx
@@ -17,7 +17,7 @@ function AttendanceSymbols({ status }: Props) {
   const property: Property = {
     color: 'bg-[#0E8E0B]',
     // icon: <i className="fa-solid fa-check font-semibold" />,
-    icon: <FaCheck className="text-[.8rem]" />,
+    icon: <FaCheck className="mt-[.1rem] md:mt-0 text-[.75rem] md:text-[.8rem]" />,
   };
 
   if (score === '1') {
@@ -26,11 +26,13 @@ function AttendanceSymbols({ status }: Props) {
   }
   if (score === '0') {
     property.color = 'bg-[#C30909]';
-    property.icon = <FaXmark className="text-base" />;
+    property.icon = (
+      <FaXmark className="-ml-[.1rem] md:ml-0 text-[.93rem] md:text-base" />
+    );
   }
   return (
     <div
-      className={`${property.color} flex items-center justify-center w-[1.5rem] h-[1.5rem] rounded-full text-white`}
+      className={`${property.color} flex items-center justify-center w-[1.3rem] h-[1.3rem] md:w-[1.5rem] md:h-[1.5rem] rounded-full text-white`}
     >
       {property.icon}
     </div>
