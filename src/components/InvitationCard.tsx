9cc94759d00290ea8ed19670d1ae492cf8535082
Grace UWICYEZA <124083824+UwicyezaG@users.noreply.github.com>
2024-10-16 10:33:15 +0200

    Update dark background color on mobile design to match on desktop (#584)

--- src/components/InvitationCard.tsx
+++ src/components/InvitationCard.tsx
@@ -13,20 +13,20 @@ interface InvitationCardType {
 // Define the InvitationCard component using a function declaration
 function InvitationCard({ icon, status, time, staticNumber, percentage }: InvitationCardType) {
   return (
-    <div className='bg-blue-100 lg:dark:bg-blue-100 sm:dark:bg-[#0A1729] xm:dark:bg-[#0A1729] w-full grid h-36 text-black shadow-lg rounded-lg m-auto'>
+    <div className='bg-blue-100 lg:dark:bg-[#0A1729] sm:dark:bg-[#0A1729] xm:dark:bg-[#0A1729] w-full grid h-36 text-black shadow-lg rounded-lg m-auto'>
 
         <div className='flex flex-row gap-3 mt-4 sm:gap-1'>
           <div className='flex w-8 h-10 ml-2'>
             {icon}
           </div>
-          <h3 className='text-md md:text-md lg:text-xl font-Inter font-bold text-[12px] sm:dark:text-white xm:dark:text-white lg:dark:text-black'>{status}</h3>
+          <h3 className='text-md md:text-md lg:text-xl font-Inter font-bold text-[12px] sm:dark:text-white xm:dark:text-white lg:dark:text-white'>{status}</h3>
         </div>
         <div>
           <p className='-mt-10 ml-12 text-[12px] font-inter text-gray-500 font-inter sm:-[8px]'>{time}</p>
         </div>
 
       <div className='flex items-center justify-center -mt-10 '>
-        <div className='font-bold -mt-4 ml-0 sm:ml-5 text-[30px] md:text-[40px] sm:dark:text-white xm:dark:text-white lg:dark:text-black'>{staticNumber}
+        <div className='font-bold -mt-4 ml-0 sm:ml-5 text-[30px] md:text-[40px] sm:dark:text-white xm:dark:text-white lg:dark:text-white'>{staticNumber}
         </div>
         <div className='text-base text-[#7258ce] ml-3 mb-4'>
           <AiOutlineRise className="text-xl" data-testid="ai-outline-user"/>
