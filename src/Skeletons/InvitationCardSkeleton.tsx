f08afd4888777824e5d42e5cdf134aff5746c8b5
Grace UWICYEZA <124083824+UwicyezaG@users.noreply.github.com>
2024-10-24 12:38:46 +0200

    fix(#595):Improve the dark mode skeleton to match dark design (#598)

--- src/Skeletons/InvitationCardSkeleton.tsx
+++ src/Skeletons/InvitationCardSkeleton.tsx
@@ -4,17 +4,38 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 function InvitationCardSkeleton() {
   return (
-    <div className="bg-white rounded-md shadow-sm p-4 w-[100%] md:w-[30%]">
-      <div className="flex flex-row items-center gap-4 mb-3">
-        <Skeleton circle width={50} height={50} />
+    <div className='bg-white dark:bg-[#0A1729] rounded-md shadow-sm p-4 w-[100%] md:w-[30%]'>
+      <div className='flex flex-row items-center gap-4 mb-3'>
+        <Skeleton
+          circle
+          width={50}
+          height={50}
+          highlightColor='#E0E0E' 
+          className="dark:bg-[#4B5563]" 
+        />
         <div>
-          <Skeleton width={100} height={20} />
+          <Skeleton
+            width={100}
+            height={20}
+            highlightColor='#E0E0E' 
+            className="dark:bg-[#4B5563]" 
+          />
         </div>
       </div>
 
-      <Skeleton width={70} height={30} />
+      <Skeleton
+        width={70}
+        height={30}
+        highlightColor='#E0E0E' 
+        className="dark:bg-[#4B5563]" 
+      />
 
-      <Skeleton width={50} height={20} />
+      <Skeleton
+        width={50}
+        height={20}
+        highlightColor='#E0E0E' 
+        className="dark:bg-[#4B5563]" 
+      />
     </div>
   );
 }
--- src/Skeletons/SkeletonTable.tsx
+++ src/Skeletons/SkeletonTable.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 interface SkeletonTableProps {
@@ -11,18 +10,30 @@ function SkeletonTable({ columns }: SkeletonTableProps) {
       <thead>
         <tr>
           {columns.map((column: any, index: React.Key | null | undefined) => (
-            <th key={column.id || column.name} className="px-6 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-              <div className="h-4 bg-gray-300 rounded w-3/4" aria-label="Loading skeleton" />
+            <th
+              key={column.id || column.name}
+              className="px-6 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider dark:bg-[#0A1729] dark:text-gray-400"
+            >
+              <div
+                className="h-4 bg-gray-300 rounded w-3/4 dark:bg-gray-700"
+                aria-label="Loading skeleton"
+              />
             </th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {[...Array(5)].map((_, rowIndex:any) => (
+        {[...Array(5)].map((_, rowIndex: any) => (
           <tr key={rowIndex.id || rowIndex.name}>
-            {columns.map((_, colIndex:any) => (
-              <td key={colIndex.id || colIndex.name} className="px-6 py-4 border-b border-gray-200 bg-white text-sm">
-                <div className="h-4 bg-gray-300 rounded w-full" aria-label="Loading skeleton" />
+            {columns.map((_, colIndex: any) => (
+              <td
+                key={colIndex.id || colIndex.name}
+                className="px-6 py-4 border-b border-gray-200 bg-white text-sm dark:bg-[#0A1731] dark:border-gray-400"
+              >
+                <div
+                  className="h-4 bg-gray-300 rounded w-full dark:bg-gray-700"
+                  aria-label="Loading skeleton"
+                />
               </td>
             ))}
           </tr>
