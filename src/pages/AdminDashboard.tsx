3786c0f13d28b167d62e5065149917251298aa6d
RWEMAREMY <rwemaremy21@gmail.com>
2024-11-14 09:10:59 +0200

    new piechart and stats updated

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -3,16 +3,19 @@ import { t } from 'i18next';
 import { useTranslation } from 'react-i18next';
 import { useMutation } from '@apollo/client';
 import { toast } from 'react-toastify';
+import PieChart from '../Chart/PieChart';
 import BarChart from '../Chart/BarChart';
-import AppointmentsChart from '../Chart/AppointmentsChart';
+import UsersChart from '../Chart/usersChart';
 // eslint-disable-next-line import/no-useless-path-segments
 import useDocumentTitle from '../hook/useDocumentTitle';
 import Comingsoon from './Comingsoon';
 import Button from '../components/Buttons';
 import { UserContext } from '../hook/useAuth';
 import { INVITE_USER_MUTATION } from '../Mutations/manageStudentMutations';
+import { FaEye } from 'react-icons/fa';
+import DashboardTableDesign from '../components/AdminDashboardTable';
 
-function SupAdDashboard() {
+function AdminDashboard() {
   const { user } = useContext(UserContext);
   const { t }: any = useTranslation();
 
@@ -25,7 +28,6 @@ function SupAdDashboard() {
   const inviteModel = () => {
     const newState = !inviteTraineeModel;
     setInviteTraineeModel(newState);
-    //  this is true
   };
 
   const [inviteUser] = useMutation(INVITE_USER_MUTATION, {
@@ -125,11 +127,14 @@ function SupAdDashboard() {
       </div>
       <div className="flex flex-col grow bg-light-bg dark:bg-dark-frame-bg">
         <div className="flex flex-col justify-between">
+          <div className="w-[80%] h-[100%] pl-[210px] pb-8  flex flex-row ">
+            <PieChart />
+          </div>
           <div className="w-[80%] h-[100%] pl-[90px] pb-8 flex flex-row ">
             <div className="flex flex-col justify-center">
               <span className="mr-[80px] rotate-90 ">Users</span>
             </div>
-            <AppointmentsChart />
+            <UsersChart />
           </div>
 
           <div className="w-[80%] h-[100%] pl-[90px] pb-8 flex flex-row ">
@@ -140,8 +145,15 @@ function SupAdDashboard() {
           </div>
         </div>
       </div>
+      <div className="w-full ">
+        <DashboardTableDesign />
+      </div>
     </>
   );
 }
 
-export default SupAdDashboard;
+export default AdminDashboard;
+
+
+
+
