3d4c0d1d39707f965fb8010530891e99d2ef21fb
Igor IHIMBAZWE <iigore2061@gmail.com>
2024-11-19 10:06:48 +0200

    fix-siderbar-scroll

--- src/components/DashHeader.tsx
+++ src/components/DashHeader.tsx
@@ -39,7 +39,7 @@ export const TICKETS_NOTS_SUB = gql`
   }
 `;
 
-function DashHeader() {
+function DashHeader({minimize}: any) {
   /* istanbul ignore next */
   const { minimized, toggleMinMenu, showNav, toggleNav } =
     useContext(MenuContext);
@@ -126,7 +126,7 @@ function DashHeader() {
           handleShowProfileDropdown={handleShowProfileDropdown}
         />
       )}
-      <div className="font-serif  transform:sc w-full py-4 z-20 bg-indigo-100 dark:bg-dark-bg page-header">
+      <div className="font-serif h-16 transform:sc w-full py-4 z-20 bg-indigo-100 dark:bg-dark-bg page-header">
         <div className="px-3 flex items-center w-full font-serif">
           <div className="flex px-5 lg:hidden">
             <div
@@ -144,7 +144,7 @@ function DashHeader() {
           </div>
           <div className="hidden lg:flex">
             <div
-              onClick={toggleMinMenu}
+              onClick={() => { minimize(!minimized); return toggleMinMenu() }}
               className="w-7"
               role="button"
               tabIndex={0}
